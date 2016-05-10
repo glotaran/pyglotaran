5df842685a20d68781134ff5a5584c2caffee0a0
Stefan Schütz <yaminokeshin@gmail.com>
2016-05-10 14:42:33 +0200

    Corrected merge error

--- large_example_irf_disp.py
+++ large_example_irf_disp.py
@@ -4,13 +4,33 @@ import scipy.linalg.lapack as lapack
 from scipy.special import erf
 import time
 
-def calculateC(k, T, mu, tau):
-    tau_tilde = tau / (2 * np.sqrt(2 * np.log(2)))
-    C = .5* np.exp(np.outer(T, -k))
-    term1 = np.exp(np.outer(k, (mu + k * tau_tilde * tau_tilde / 2)))
-    term2 = 1 + erf()
-    C = np.dot(C, term1)
+
+def disp(par_0, par_disp=None, l=None, l_c=None):
+    disp_s = par_0
+    if par_disp:
+        if not l or not l_c:
+            raise ValueError("Parameters missing")
+        for i in range(par_disp.shape[0]):
+            disp_s += par_disp[i] * np.power(((l - l_c) / 100), i)
+    return disp_s
+
+
+def calculateC(k, T, mu_0=None, delta_0=None, mu_disp=None, delta_disp=None, l=None, l_c_mu=None, l_c_delta=None):
+    C = np.exp(np.outer(T, -k))
+    if not mu_0 and not delta_0:
+        return C
+    mu = disp(mu_0, mu_disp, l, l_c_mu)
+    delta = disp(delta_0, delta_disp, l, l_c_delta)
+    delta_tilde = delta / (2 * np.sqrt(2 * np.log(2)))
+    for i in range(T.shape[0]):
+        for j in range(k.shape[0]):
+            t_i = T[i]
+            k_j = k[j]
+            term_1 = np.exp(k_j * (k_j * delta_tilde * delta_tilde / 2))
+            term_2 = 1 + erf((t_i - (mu + k_j * delta_tilde * delta_tilde)) / (np.sqrt(2) * delta_tilde))
+            C[i, j] *= .5 * term_1 * term_2
     return C
+
 
 def qr(a, c):
     qr, tau, _, _ = lapack.dgeqrf(a, overwrite_a=1)
@@ -21,13 +41,36 @@ def qr(a, c):
     return c
 
 
-def solve(params, PSI, times, n_k):
+def solve(params, PSI, times, n_k, irf=False, disp=False, n_m=0, n_d=0):
     res = np.empty(PSI.shape, dtype=np.float64)
+    mu_0 = None
+    delta_0 = None
+    mu_disp = None
+    delta_disp = None
+    l = None
+    l_c_mu = None
+    l_c_delta = None
     k = np.asarray(params[:n_k])
-    mu = params[n_k]
-    tau = params[n_k + 1]
-    C = calculateC(k, times, mu, tau) #This is just an optimization for wavelength independent C matrices
+    if irf:
+        idx = n_k
+        mu_0 = params[idx]
+        idx += 1
+        delta_0= params[idx]
+        if disp:
+            l_c_mu = params[idx]
+            idx += 1
+            mu_disp = np.asarray(params[idx: idx + n_m])
+
+            idx += n_m
+
+            l_c_delta = params[idx]
+            idx += 1
+            delta_disp = np.asarray(params[idx: idx + n_d])
+
     for i in range(PSI.shape[1]):
+        if disp:
+            l = i
+        C = calculateC(k, times, mu_0, delta_0, mu_disp, delta_disp, l, l_c_mu, l_c_delta)
         b = PSI[:,i]
         res[:,i] = qr(C, b)
 
@@ -61,7 +104,7 @@ def main():
 
     start = time.perf_counter()
 
-    res = scipy.optimize.least_squares(solve, params, args=(PSI, times, start_kinpar.shape[0]), verbose=0, method='trf')
+    res = scipy.optimize.least_squares(solve, params, args=(PSI, times, start_kinpar.shape[0], True), verbose=0, method='trf')
 
     stop = time.perf_counter()
 
