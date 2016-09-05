93b67aeb466b956cb63a49dd59bed4f5ae6c5f06
Joern Weissenborn <joern.weissenborn@gmail.com>
2016-09-05 18:14:29 +0200

    Restructering and reqrite of setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,39 @@
 from distutils.core import setup
 from distutils.extension import Extension
+# TODO: include generated c and include switches if cython is not available ->
+# https://stackoverflow.com/questions/4505747/how-should-i-structure-a-python-package-that-contains-cython-code
 from Cython.Distutils import build_ext
+# TODO: bootstrap numpy ->
+# https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
 import numpy
 
-ext_modules=[
+ext_modules = [
     Extension("calculateC",
-              ["calculateC.pyx"],
+              ["glotaran-core/calculateC.pyx"],
               include_dirs=[numpy.get_include()],
               libraries=["m"],
-              extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp" ],
-              extra_link_args=['-fopenmp']
-              #define_macros=[("CYTHON_TRACE_NOGIL", "1")]
-              ),
-#    Extension("solve",
-#              ["solve.pyx"],
-#              include_dirs=[numpy.get_include()],
-#              libraries=["m"],
-#              extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp" ],
-#              extra_link_args=['-fopenmp']
-#              #define_macros=[("CYTHON_TRACE_NOGIL", "1")]
-#              )
-]
+              extra_compile_args=["-O3", "-ffast-math", "-march=native",
+                                  "-fopenmp"],
+              extra_link_args=['-fopenmp'])
+              ]
 
 
 setup(
-    name = "large_example_irf_disp",
-    cmdclass = {"build_ext": build_ext},
-    ext_modules = ext_modules
+    name="glotaran-core",
+    version="0.1.0",
+    description='The Glotaran fitting engine.',
+    url='http://github.com/ThingiverseIO/pythingiverseio',
+    author='Joris Snellenburg, Stefan Schuetz, Joern Weissenborn',
+    author_email="""xxjorisxx@gmail.com,
+                    YamiNoKeshin@gmail.com,
+                    joern.weissenborn@gmail.com""",
+    license='GPLv3',
+    packages=['glotaran-core'],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'lmfit',
+    ],
+    cmdclass={"build_ext": build_ext},
+    ext_modules=ext_modules,
 )
