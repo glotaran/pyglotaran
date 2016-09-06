ceb5a5fdbe2408089e6f1ccaa37e4836b84bbebf
Joern Weissenborn <joern.weissenborn@gmail.com>
2016-09-06 18:00:52 +0200

    Datasets now takes a dataset or a list of dataset as constructor.
    referencing #2

--- glotaran_core/global_analysis.py
+++ glotaran_core/global_analysis.py
@@ -1,20 +1,26 @@
 from datasets import Datasets
+from dataset import Dataset
+from models import Models
+from model import Model
+
 
 class GlobalAnalysis(object):
     """
     Represents a global analysis job.
 
-    Parameter: A Datasets object representing the datasets to be fitted and a
-    Models object representing the fitmodels.
+    Parameter: A Dataset or Datasets object representing the dataset(s) to be
+    fitted and a Model or Models object representing the fitmodel(s).
 
     Datasets and models are linked by labels.
     """
     def __init__(self, datasets, models):
-        
 
+        if isinstance(datasets, Dataset):
+            ds = Datasets()
+            ds.add(datasets)
+            dataset = ds
         if not isinstance(datasets, Datasets):
             raise TypeError
-
 
         self._datasets = datasets
         self._model = model
