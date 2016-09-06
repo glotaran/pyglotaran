83fee4e1780b1aeb09f40c1373f40f719b8a2f56
Joern Weissenborn <joern.weissenborn@gmail.com>
2016-09-06 18:02:08 +0200

    Created Model and Models class.
    The Model class is not an implentation lmfit.Model any more but shall rather be used to construct one.
    
    referencing #2

---
+++ glotaran_core/models.py
@@ -0,0 +1,35 @@
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
+from model import Model
+
+
+class Models(OrderedDict):
+    """
+    A dictionary of all models to be fitted.
+    """
+    def __init__(self, models):
+        super(Models, self)
+        if not isinstance(models, list):
+            models = [models]
+        if any(not isinstance(model, Model) for model in models):
+            raise TypeError
+
+        for model in models:
+            self.add(model)
+
+    def add(self, model):
+        """
+        Add a model.
+        """
+        if not isinstance(model, Model):
+            raise TypeError
+
+        if len(model.labels()) != set(model.labels()) or any(label in self for
+                                                             label in
+                                                             model.labels()):
+            raise Exception("Labels must be unique")
+
+        for label in model.labels:
+            self.__setitem__(label, model)
