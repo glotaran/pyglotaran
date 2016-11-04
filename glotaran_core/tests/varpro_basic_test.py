ff6951ff1d7b330b56b7ae920d799583882d5644
Joris Snellenburg <j.snellenburg@vu.nl>
2016-11-04 23:13:43 +0100

    Updated basic varpro test to reflect R test

--- glotaran_core/tests/varpro_basic_test.py
+++ glotaran_core/tests/varpro_basic_test.py
@@ -7,22 +7,20 @@ def calculateC(k, T):
     return np.exp(np.outer(T, -k))
 
 
-def qr(a, c):
+def qr(a, c):    
+    # TODO: audit this routine
     qr, tau, _, _ = lapack.dgeqrf(a, overwrite_a=1)
     c, _, _ = lapack.dormqr("L", "T", qr, tau, c, max(1, a.shape[1]), overwrite_c=1)
     for i in range(a.shape[1]):
         c[i] = 0
-    c, _, _ = lapack.dormqr("L", "N", qr, tau, c, max(1, a.shape[1]), overwrite_c=1)
+    c, _, _ = lapack.dormqr("L", "N", qr, tau, c, max(1, a.shape[1]), overwrite_c=1)    
     return c
 
 
 def solve(k, single_trace, times):
     res = np.empty(single_trace.shape, dtype=np.float64)
-    C = calculateC(k, times)
-    assert len(single_trace.shape)==1, "data is not a single trace"
-        
+    C = calculateC(k, times)            
     res = qr(C, single_trace)
-
     return res.flatten()
 
 
@@ -30,17 +28,18 @@ def main():
 
     times = np.asarray(np.arange(0, 1500, 1.5))
     amp = np.asarray([1,2])
-    kinpar = np.asarray([.006667,0.0303])
+    kinpar = np.asarray([0.0101,.00202])
 
     C = calculateC(kinpar, times)
 
     single_trace = np.dot(C, amp)
+    assert len(single_trace.shape)==1, "data is not a single trace"
 
-    start_kinpar = np.asarray([.005, 0.02])
+    start_kinpar =  0.1*kinpar
     start = time.perf_counter()
 
     res = scipy.optimize.least_squares(solve, start_kinpar, args=(single_trace, times), verbose=2, method='lm')
-
+    #TODO: also get amplitudes from varpro out, not just parameter estimates
     stop = time.perf_counter()
 
     print(stop - start)
