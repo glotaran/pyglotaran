3f1726de607226ad07b90461c31308c21dac6726
Joern Weissenborn <joern.weissenborn@gmail.com>
2016-11-30 18:11:08 +0100

    added optional noise to model evalutation

--- glotaran_core/fitting/variable_projection/seperable_model.py
+++ glotaran_core/fitting/variable_projection/seperable_model.py
@@ -13,12 +13,19 @@ class SeperableModel(object):
     def eval(self, parameter, *args, **kwargs):
         e = self.e_matrix(**kwargs)
         c = self.c_matrix(parameter, *args, **kwargs)
-        return np.dot(c, np.transpose(e))
+        noise = kwargs["noise"] if "noise" in kwargs else False
+        res = np.dot(c, np.transpose(e))
+        if noise:
+            std_dev = kwargs["noise_std_dev"] if "noise_std_dev" in kwargs \
+                else 1.0
+            res = np.random.normal(res, std_dev)
+        return res
 
     def fit(self, initial_parameter, *args, **kwargs):
         minimizer = VariableProjectionMinimizer(self, initial_parameter, *args,
                                                 **kwargs)
+        verbose = kwargs['verbose'] if 'verbose' in kwargs else 2
         return minimizer.minimize(method='least_squares',
                                   ftol=1e-10,
                                   gtol=1e-10,
-                                  verbose=2)
+                                  verbose=verbose)
