de8df60d83ebbd806bc75ebfbd0511305d84cba6
Joris Snellenburg <j.snellenburg@vu.nl>
2017-03-01 19:41:14 +0100

    Extended api test to 2D 1D case needs to be evaluated against 2D case at a single wavelength

--- tests/python/test_glotaran_api.py
+++ tests/python/test_glotaran_api.py
@@ -9,14 +9,14 @@ from glotaran.models.kinetic import KineticModel, KMatrix, KineticMegacomplex, K
 import numpy as np
 import matplotlib.pyplot as plt
 model = KineticModel()
-plist = create_parameter_list([["k1", 0.05], ["k2", 100]])
+plist = create_parameter_list([["k1", 0.002], ["k2", 0.002]])
 model.parameter = plist
 model.add_parameter(Parameter(1, label="j1"))
 model.add_parameter(Parameter(1, label="j2"))
 
 model.compartments = ["s1","s2"]
 
-k_mat = KMatrix("k1", {("s1","s2"): 1, ("s2","s2"): 2})
+k_mat = KMatrix("k1", {("s1","s1"): 1, ("s2","s1"): 2})
 
 model.add_k_matrix(k_mat)
 
@@ -35,13 +35,21 @@ model.add_dataset(dset)
 print(model)
 
 times = np.asarray(np.arange(0, 1500, 1.5))
+test_x = np.arange(12820, 15120, 46)
 
 fitmodel = KineticSeperableModel(model)
-data = fitmodel.eval(fitmodel.get_initial_fitting_parameter(), *times, **{'dataset':'d1', 'd1_x':[0,1,2]})
+data = fitmodel.eval(fitmodel.get_initial_fitting_parameter(), *times, **{'dataset':'d1',
+                                           'noise':True, 'noise_std_dev':0.0001,
+                                           'd1_x': test_x,
+                                           'amplitudes':[10, 20],
+                                          'locations':[14700, 13515],
+                                           'delta': [50,100]})
+#plt.plot(times, data)
+fig = plt.figure()
+plt.xlabel('Time (ps)')
+plt.ylabel('$Wavenumber\ [\ cm^{-1}\ ]$')
+plt.pcolormesh(times, test_x, data.T)
+plt.show()
+# data = fitmodel.eval(fitmodel.get_initial_fitting_parameter(), *times, **{'dataset':'d1', 'd1_x':[0,1,2]})
 #plt.plot(times, data[:, 0])
 print(data.shape)
-plt.plot(times, data)
-plt.ioff()
-plt.show()
-
-
