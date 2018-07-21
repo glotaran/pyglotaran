954229250a86e9ac1cd95ad0d115a8df02a843fc
Joern Weissenborn <joern.weissenborn@gmail.com>
2018-07-21 19:47:23 +0200

    Added getters for das and concentration in `fitmodel.KineticResult`

--- glotaran/models/spectral_temporal/result.py
+++ glotaran/models/spectral_temporal/result.py
@@ -6,4 +6,37 @@ from glotaran.fitmodel import Result
 
 
 class KineticResult(Result):
-    pass
+    """The result of kinetic model fit"""
+
+    def decay_associated_spectra(self, dataset: str) -> np.array:
+        """
+
+        Parameters
+        ----------
+        dataset: str
+            Label of the dataset.
+
+
+        Returns
+        -------
+        das : numpy.array
+            Decay Associated Spectra
+        """
+        return self.estimated_matrix(dataset)
+
+    def concentraions(self, dataset: str) -> np.array:
+        """
+
+        Parameters
+        ----------
+        dataset: str
+                Label of the dataset.
+
+
+        Returns
+        -------
+        concentraions : numpy.array
+            Concentrations
+
+        """
+        return self.calculated_matrix(dataset)
