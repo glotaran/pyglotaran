8100ab7a1aee80d55e61000bbeddd108031a8d73
Joern Weissenborn <joern.weissenborn@gmail.com>
2018-08-07 14:54:44 +0200

    deleted tests and notebooks

--- tests/R/_load_TIMP_package_and_set_seed.R
+++
@@ -1,6 +0,0 @@
-## If the R package TIMP is not installed, please run:
-## install.packages("TIMP")
-
-require(TIMP) 
-set.seed(123)
-sigma <- .01
--- tests/R/_shared_wavenumber_axis.R
+++
@@ -1,2 +0,0 @@
-wavenum <- seq(12820, 15120, by=4.6)
-print(sprintf("length(wavenum) = %i ", length(wavenum)))
