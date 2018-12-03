b89b26bec03ed9cc03869344411d1edbf095bc3f
Joern Weissenborn <joern.weissenborn@gmail.com>
2018-12-03 14:22:24 +0100

    Added benchmark

---
+++ glotaran/models/doas/test/test_doas_benchmark.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from glotaran import ParameterGroup
+from glotaran.models.doas import DOASModel
+from glotaran.models.doas.doas_matrix import calculate_doas_matrix
+
+
+def test_doas_benchmark(benchmark):
+    model = DOASModel.from_dict({
+        'initial_concentration': {
+            'j1': {
+                'compartments': ['s1', 's2'],
+                'parameters': ['j.1', 'j.0']
+            },
+        },
+        'oscillation': {
+            'osc1': {'frequency': 'osc.freq', 'rate': 'osc.rate'},
+            'osc2': {'frequency': 'osc.freq', 'rate': 'osc.rate'}
+        },
+        'megacomplex': {
+            'm1': {
+                'oscillation': ['osc1', 'osc2'],
+            }
+        },
+        'irf': {
+            'irf1': {'type': 'gaussian', 'center': 'irf.center', 'width': 'irf.width'},
+        },
+        'dataset': {
+            'dataset1': {
+                'initial_concentration': 'j1',
+                'megacomplex': ['m1'],
+                'irf': 'irf1',
+            }
+        }
+    })
+    parameter = ParameterGroup.from_dict({
+        'j': [
+            ['1', 1, {'vary': False}],
+            ['0', 0, {'vary': False}],
+        ],
+        'osc': [
+            ['freq', 16],
+            ['rate', 0.3],
+        ],
+        'irf': [['center', 0.5], ['width', 0.3]],
+    })
+    time = np.arange(0, 6, 0.001)
+    dataset = model.dataset['dataset1'].fill(model, parameter)
+
+    # benchmark something
+    benchmark(calculate_doas_matrix, dataset, 0, time)
