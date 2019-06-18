17b6199e252e7bc6d762445d5ba5b439a15c6341
Joern Weissenborn <joern.weissenborn@gmail.com>
2019-06-18 14:43:14 +0200

    replaced doas matrix with pure numpy

--- glotaran/models/doas/doas_matrix.py
+++ glotaran/models/doas/doas_matrix.py
@@ -1,6 +1,7 @@
 """Glotaran DOAS Matrix"""
 
 import numpy as np
+from scipy.special import erf
 
 from glotaran.models.spectral_temporal.kinetic_matrix import calculate_kinetic_matrix
 from glotaran.models.spectral_temporal.irf import IrfGaussian
@@ -11,48 +12,61 @@ from doas_matrix_faddeva import calc_doas_matrix_faddeva
 def calculate_doas_matrix(dataset_descriptor=None, axis=None, index=None, irf=None):
 
     oscillations = _collect_oscillations(dataset_descriptor)
-
-    matrix = np.zeros((axis.size, 2 * len(oscillations)), dtype=np.float64)
+    matrix = np.zeros((axis.size, len(2 * oscillations)), dtype=np.float64)
     clp = []
-    if not len(oscillations) == 0:
-        for oscillation in oscillations:
-            clp.append(f'{oscillation.label}_cos')
-            clp.append(f'{oscillation.label}_sin')
 
-        delta = np.abs(axis[1:] - axis[:-1])
-        delta_min = delta[np.argmin(delta)]
-        frequency_max = 1 / (2 * 0.03 * delta_min)
+    delta = np.abs(axis[1:] - axis[:-1])
+    delta_min = delta[np.argmin(delta)]
+    frequency_max = 1 / (2 * 0.03 * delta_min)
+
+    idx = 0
+    for oscillation in oscillations:
+        clp.append(f'{oscillation.label}_cos')
+        clp.append(f'{oscillation.label}_sin')
+
+        # convert from cm^-1 to ps^-1
+        frequency = oscillation.frequency * 0.03 * 2 * np.pi
+        if frequency >= frequency_max:
+            frequency = np.mod(frequency, frequency_max)
 
         if dataset_descriptor.irf is None:
-            idx = 0
-            for oscillation in oscillations:
-
-                # convert from cm^-1 to ps^-1
-                frequency = oscillation.frequency * 0.03 * 2 * np.pi
-                if frequency >= frequency_max:
-                    frequency = np.mod(frequency, frequency_max)
-
-                osc = np.exp(-oscillation.rate * axis - 1j * frequency * axis)
-                matrix[:, idx] = osc.real
-                matrix[:, idx + 1] = osc.imag
-                idx += 2
+            osc = np.exp(-oscillation.rate * axis - 1j * frequency * axis)
         elif isinstance(dataset_descriptor.irf, IrfGaussian):
 
             centers, widths, scales, backsweep, backsweep_period = \
                     dataset_descriptor.irf.parameter(index)
 
-            rates = np.array([osc.rate for osc in oscillations])
-            frequencies = np.array([osc.frequency * 0.03 * 2 * np.pi for osc in oscillations])
+            osc = np.zeros_like(axis, dtype=np.complex64)
 
-            over_max = frequencies >= frequency_max
-            frequencies[over_max] = np.mod(frequencies[over_max], frequency_max)
+            for i in range(len(centers)):
+                shifted_axis = axis - centers[i]
+                d = widths[i]**2
+                k = (oscillation.rate + 1j * frequency)
 
-            for i, _ in enumerate(centers):
-                calc_doas_matrix_faddeva(matrix, frequencies, rates, axis,
-                                         centers[i], widths[i], scales[i])
-            matrix /= np.sum(scales)
+                a = (-1 * shifted_axis + 0.5 * d * k) * k
+                a = np.minimum(a, 709)
+                a = np.exp(a)
+                b = 1 + erf((shifted_axis - d * k) / (np.sqrt(2) * widths[i]))
+                if not all(np.isfinite(a)):
+                    raise Exception("Non numeric values in term 'a' for oscillation with frequency"
+                                    f"'{osc.frequency}', '{osc.rates}'")
+                if not all(np.isfinite(b)):
+                    idx = np.where(np.logical_not(np.isfinite(b)))[0]
+                    raise Exception(f"""Non numeric values in term 'b' for oscillation '{oscillation.label}':
+                                    frequency: {frequency}
+                                    rate: {oscillation.rate}
+                                    center: {centers[i]}
+                                    width: {widths[i]}
+                                    frequency_max: {frequency_max}
+                                    delta_min: {delta_min}
+                                    nonfinite timpoints: {axis[idx]} """)
+                osc += a * b * scales[i]
+            osc /= np.sum(scales)
+        matrix[:, idx] = osc.real
+        matrix[:, idx + 1] = osc.imag
+        idx += 2
 
-    kinetic_clp, kinetic_matrix = calculate_kinetic_matrix(dataset_descriptor, axis, index, irf)
+    kinetic_clp, kinetic_matrix = calculate_kinetic_matrix(dataset_descriptor, axis, index)
     if kinetic_matrix is not None:
         clp = clp + kinetic_clp
         matrix = np.concatenate((matrix, kinetic_matrix), axis=1)
