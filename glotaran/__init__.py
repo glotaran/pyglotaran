1b3e508e282531b943b28992ee95ac508835ded3
Joris Snellenburg <jsnel@users.noreply.github.com>
2020-02-25 01:43:53 +0100

    Revert "Bump version to 0.1"
    This reverts commit 1496f284d0b06c80647b7a1c4cadb6ca84792209.

--- glotaran/__init__.py
+++ glotaran/__init__.py
@@ -2,7 +2,7 @@
 
 from . import model, parameter, io  # noqa: F401
 
-__version__ = '0.1.0'
+__version__ = '0.0.15'
 
 ParameterGroup = parameter.ParameterGroup
 
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ entry_points = """
 
 setup(
     name="glotaran",
-    version='0.1.0',
+    version='0.0.15',
     description='The Glotaran fitting engine.',
     long_description=long_description,
     long_description_content_type="text/markdown",
