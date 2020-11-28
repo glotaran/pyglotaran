f54b08484b75a97b2d4fda75b98e446a4320040e
s-weigand <s.weigand.phy@gmail.com>
2020-11-28 17:00:54 +0100

    Added stub for spectral_relations

---
+++ glotaran/builtin/models/kinetic_spectrum/spectral_relations.pyi
@@ -0,0 +1,61 @@
+from typing import Any
+from typing import List
+from typing import Tuple
+
+import numpy as np
+
+from glotaran.model import model_attribute
+from glotaran.parameter import Parameter
+from glotaran.parameter import ParameterGroup
+
+from .kinetic_spectrum_model import KineticSpectrumModel
+
+class SpectralRelation:
+    @property
+    def compartment(self) -> str:
+        ...
+
+    @property
+    def target(self) -> str:
+        ...
+
+    @property
+    def parameter(self) -> Parameter:
+        ...
+
+    @property
+    def interval(self) -> List[Tuple[float, float]]:
+        ...
+
+    def applies(self, index: Any) -> bool:
+        ...
+
+
+def create_spectral_relation_matrix(
+    model: KineticSpectrumModel,
+    parameter: ParameterGroup,
+    clp_labels: List[str],
+    matrix: np.ndarray,
+    index: float,
+) -> Tuple[List[str], np.ndarray]:
+    ...
+
+
+def apply_spectral_relations(
+    model: KineticSpectrumModel,
+    parameter: ParameterGroup,
+    clp_labels: List[str],
+    matrix: np.ndarray,
+    index: float,
+) -> Tuple[List[str], np.ndarray]:
+    ...
+
+
+def retrieve_related_clps(
+    model: KineticSpectrumModel,
+    parameter: ParameterGroup,
+    clp_labels: List[str],
+    clps: np.ndarray,
+    index: float,
+) -> Tuple[List[str], np.ndarray]:
+    ...
