d0f96cf68a526e67f17c67664a1668a54b4171a7
s-weigand <s.weigand.phy@gmail.com>
2020-11-28 15:20:52 +0100

    Added stub for k_matrix

---
+++ glotaran/builtin/models/kinetic_image/k_matrix.pyi
@@ -0,0 +1,56 @@
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Tuple
+
+import numpy as np
+
+from glotaran.model import model_attribute
+from glotaran.parameter import Parameter
+
+from .initial_concentration import InitialConcentration
+
+class KMatrix:
+    @classmethod
+    def empty(cls: Any, label: str, compartments: List[str]) -> KMatrix:
+        ...
+
+    def involved_compartments(self) -> List[str]:
+        ...
+
+    def combine(self, k_matrix: KMatrix) -> KMatrix:
+        ...
+
+    def matrix_as_markdown(self, compartments: List[str] = ..., fill_parameter: bool = ...) -> str:
+        ...
+
+    def a_matrix_as_markdown(self, initial_concentration: InitialConcentration) -> str:
+        ...
+
+    def reduced(self, compartments: List[str]) -> np.ndarray:
+        ...
+
+    def full(self, compartments: List[str]) -> np.ndarray:
+        ...
+
+    def eigen(self, compartments: List[str]) -> Tuple[np.ndarray, np.ndarray]:
+        ...
+
+    def rates(self, initial_concentration: InitialConcentration) -> np.ndarray:
+        ...
+
+    def a_matrix(self, initial_concentration: InitialConcentration) -> np.ndarray:
+        ...
+
+    def a_matrix_non_unibranch(self, initial_concentration: InitialConcentration) -> np.ndarray:
+        ...
+
+    def a_matrix_unibranch(self, initial_concentration: InitialConcentration) -> np.array:
+        ...
+
+    def is_unibranched(self, initial_concentration: InitialConcentration) -> bool:
+        ...
+
+    @property
+    def matrix(self) -> Dict[Tuple[str, str], Parameter]:
+        ...
