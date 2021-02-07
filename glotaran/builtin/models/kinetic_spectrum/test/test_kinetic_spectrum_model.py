68e2ff619e07190c83864dc102a04bd0e730d135
Joris Snellenburg <jsnel@users.noreply.github.com>
2021-02-07 15:19:51 +0100

    Finished refactor of test_kinetic_spectrum_model.py
    Made the model reusable between fitting and simulation. Where the shapes specification is ignored during fitting and only used in simulation.

--- glotaran/builtin/models/kinetic_spectrum/test/test_kinetic_spectrum_model.py
+++ glotaran/builtin/models/kinetic_spectrum/test/test_kinetic_spectrum_model.py
@@ -6,12 +6,14 @@ from glotaran import read_parameters_from_yaml
 from glotaran.analysis.optimize import optimize
 from glotaran.analysis.scheme import Scheme
 
-MODEL_ONE_COMPONENT_BASE = """\
+MODEL_1C_BASE = """\
 type: kinetic-spectrum
 dataset:
     dataset1: &dataset1
         megacomplex: [mc1]
         initial_concentration: j1
+        shape:
+            s1: sh1
 initial_concentration:
     j1:
         compartments: [s1]
@@ -23,38 +25,28 @@ k_matrix:
     k1:
         matrix:
             (s1, s1): "2"
-"""
-MODEL_ONE_COMPONENT = MODEL_ONE_COMPONENT_BASE
-
-MODEL_SIM_ONE_COMPONENT = f"""\
-{MODEL_ONE_COMPONENT_BASE}
-dataset:
-    dataset1:
-        <<: *dataset1
-        shape:
-            s1: sh1
 shape:
     sh1:
         type: one
 """
+MODEL_1C_NO_IRF = MODEL_1C_BASE
 
-PARAMETERS_BASE = """\
+PARAMETERS_1C_NO_IRF_BASE = """\
 - [1, {"vary": False, "non-negative": False}]
 """
 
-PARAMETERS_ONE_COMPONENT_INITIAL = f"""\
-{PARAMETERS_BASE}
+PARAMETERS_1C_INITIAL = f"""\
+{PARAMETERS_1C_NO_IRF_BASE}
 - 101e-4
 """
 
-PARAMETERS_ONE_COMPONENT_WANTED = f"""\
-{PARAMETERS_BASE}
+PARAMETERS_1C_WANTED = f"""\
+{PARAMETERS_1C_NO_IRF_BASE}
 - 101e-3
 """
 
-
-MODEL_ONE_COMPONENT_ONE_CHANNEL_GASSIAN = f"""\
-{MODEL_ONE_COMPONENT_BASE}
+MODEL_1C_GAUSSIAN_IRF = f"""\
+{MODEL_1C_BASE}
 irf:
     irf1:
         type: spectral-gaussian
@@ -66,28 +58,15 @@ dataset:
         irf: irf1
 """
 
-MODEL_SIM_ONE_COMPONENT_ONE_CHANNEL_GASSIAN = f"""\
-{MODEL_ONE_COMPONENT_ONE_CHANNEL_GASSIAN}
-dataset:
-    dataset1:
-        <<: *dataset1
-        irf: irf1
-        shape:
-            s1: sh1
-shape:
-    sh1:
-        type: one
-"""
-
-PARAMETERS_ONE_COMPONENT_ONE_CHANNEL_GASSIAN_INITIAL = f"""\
-{PARAMETERS_BASE}
-- 101e-4
+PARAMETERS_1C_GAUSSIAN_IRF_INITIAL = f"""\
+{PARAMETERS_1C_NO_IRF_BASE}
+- 100e-4
 - 0.1
 - 1
 """
 
-PARAMETERS_ONE_COMPONENT_ONE_CHANNEL_GASSIAN_WANTED = f"""\
-{PARAMETERS_BASE}
+PARAMETERS_1C_GAUSSIAN_WANTED = f"""\
+{PARAMETERS_1C_NO_IRF_BASE}
 - 101e-3
 - 0.3
 - 2
@@ -95,24 +74,18 @@ PARAMETERS_ONE_COMPONENT_ONE_CHANNEL_GASSIAN_WANTED = f"""\
 
 
 class OneComponentOneChannel:
-    model = read_model_from_yaml(MODEL_ONE_COMPONENT)
-    sim_model = read_model_from_yaml(MODEL_SIM_ONE_COMPONENT)
-    initial_parameters = read_parameters_from_yaml(PARAMETERS_ONE_COMPONENT_INITIAL)
-    wanted_parameters = read_parameters_from_yaml(PARAMETERS_ONE_COMPONENT_WANTED)
+    model = read_model_from_yaml(MODEL_1C_NO_IRF)
+    initial_parameters = read_parameters_from_yaml(PARAMETERS_1C_INITIAL)
+    wanted_parameters = read_parameters_from_yaml(PARAMETERS_1C_WANTED)
     time = np.asarray(np.arange(0, 50, 1.5))
     spectral = np.asarray([0])
     axis = {"time": time, "spectral": spectral}
 
 
 class OneComponentOneChannelGaussianIrf:
-    model = read_model_from_yaml(MODEL_ONE_COMPONENT_ONE_CHANNEL_GASSIAN)
-    sim_model = read_model_from_yaml(MODEL_SIM_ONE_COMPONENT_ONE_CHANNEL_GASSIAN)
-    initial_parameters = read_parameters_from_yaml(
-        PARAMETERS_ONE_COMPONENT_ONE_CHANNEL_GASSIAN_INITIAL
-    )
-    wanted_parameters = read_parameters_from_yaml(
-        PARAMETERS_ONE_COMPONENT_ONE_CHANNEL_GASSIAN_WANTED
-    )
+    model = read_model_from_yaml(MODEL_1C_GAUSSIAN_IRF)
+    initial_parameters = read_parameters_from_yaml(PARAMETERS_1C_GAUSSIAN_IRF_INITIAL)
+    wanted_parameters = read_parameters_from_yaml(PARAMETERS_1C_GAUSSIAN_WANTED)
     time = np.asarray(np.arange(-10, 50, 1.5))
     spectral = np.asarray([0])
     axis = {"time": time, "spectral": spectral}
@@ -125,6 +98,10 @@ dataset:
         megacomplex: [mc1]
         initial_concentration: j1
         irf: irf1
+        shape:
+            s1: sh1
+            s2: sh2
+            s3: sh3
 megacomplex:
     mc1:
         k_matrix: [k1]
@@ -133,6 +110,22 @@ irf:
         type: spectral-multi-gaussian
         center: [irf.center]
         width: [irf.width]
+shape:
+    sh1:
+        type: gaussian
+        amplitude: shapes.amps.1
+        location: shapes.locs.1
+        width: shapes.width.1
+    sh2:
+        type: gaussian
+        amplitude: shapes.amps.2
+        location: shapes.locs.2
+        width: shapes.width.2
+    sh3:
+        type: gaussian
+        amplitude: shapes.amps.3
+        location: shapes.locs.3
+        width: shapes.width.3
 """
 
 MODEL_3C_PARALLEL = f"""\
@@ -163,59 +156,36 @@ k_matrix:
             (s3, s3): "kinetic.3"
 """
 
-MODEL_3C_SIM_BASE = """\
-dataset:
-    dataset1:
-        <<: *dataset1
-        shape:
-            s1: sh1
-            s2: sh2
-            s3: sh3
-shape:
-    sh1:
-        type: gaussian
-        amplitude: shapes.amps.1
-        location: shapes.locs.1
-        width: shapes.width.1
-    sh2:
-        type: gaussian
-        amplitude: shapes.amps.2
-        location: shapes.locs.2
-        width: shapes.width.2
-    sh3:
-        type: gaussian
-        amplitude: shapes.amps.3
-        location: shapes.locs.3
-        width: shapes.width.3
-"""
-
-MODEL_SIM_3C_PARALLEL = f"""\
-{MODEL_3C_PARALLEL}
-{MODEL_3C_SIM_BASE}
-"""
-
-MODEL_SIM_3C_SEQUENTIAL = f"""\
-{MODEL_3C_SEQUENTIAL}
-{MODEL_3C_SIM_BASE}
-"""
-
-PARAMETERS_3C_BASE_PARALLEL = """\
+PARAMETERS_3C_BASE = """\
 irf:
     - ["center", 1.3]
     - ["width", 7.8]
 j:
     - ["1", 1, {"vary": False, "non-negative": False}]
+    - ["0", 0, {"vary": False, "non-negative": False}]
 """
 
-PARAMETERS_3C_SIM_PARALLEL = f"""\
-kinetic:
-    - ["1", 301e-3]
-    - ["2", 502e-4]
-    - ["3", 705e-5]
+PARAMETERS_3C_BASE_PARALLEL = f"""\
+{PARAMETERS_3C_BASE}
 shapes:
     amps: [7, 3, 30, {{"vary": False}}]
     locs: [620, 670, 720, {{"vary": False}}]
     width: [10, 30, 50, {{"vary": False}}]
+"""
+
+PARAMETERS_3C_BASE_SEQUENTIAL = f"""\
+{PARAMETERS_3C_BASE}
+shapes:
+    amps: [3, 1, 5, {{"vary": False}}]
+    locs: [620, 670, 720, {{"vary": False}}]
+    width: [10, 30, 50, {{"vary": False}}]
+"""
+
+PARAMETERS_3C_PARALLEL_WANTED = f"""\
+kinetic:
+    - ["1", 301e-3]
+    - ["2", 502e-4]
+    - ["3", 705e-5]
 {PARAMETERS_3C_BASE_PARALLEL}
 """
 
@@ -227,24 +197,11 @@ kinetic:
 {PARAMETERS_3C_BASE_PARALLEL}
 """
 
-PARAMETERS_3C_BASE_SEQUENTIAL = """\
-irf:
-    - ["center", 1.3]
-    - ["width", 7.8]
-j:
-    - ["1", 1, {"vary": False, "non-negative": False}]
-    - ["0", 0, {"vary": False, "non-negative": False}]
-"""
-
 PARAMETERS_3C_SIM_SEQUENTIAL = f"""\
 kinetic:
     - ["1", 501e-3]
     - ["2", 202e-4]
     - ["3", 105e-5]
-shapes:
-    amps: [3, 1, 5, {{"vary": False}}]
-    locs: [620, 670, 720, {{"vary": False}}]
-    width: [10, 30, 50, {{"vary": False}}]
 {PARAMETERS_3C_BASE_SEQUENTIAL}
 """
 
@@ -260,9 +217,8 @@ kinetic:
 
 class ThreeComponentParallel:
     model = read_model_from_yaml(MODEL_3C_PARALLEL)
-    sim_model = read_model_from_yaml(MODEL_SIM_3C_PARALLEL)
     initial_parameters = read_parameters_from_yaml(PARAMETERS_3C_INITIAL_PARALLEL)
-    wanted_parameters = read_parameters_from_yaml(PARAMETERS_3C_SIM_PARALLEL)
+    wanted_parameters = read_parameters_from_yaml(PARAMETERS_3C_PARALLEL_WANTED)
     time = np.arange(-10, 100, 1.5)
     spectral = np.arange(600, 750, 10)
     axis = {"time": time, "spectral": spectral}
@@ -270,7 +226,6 @@ class ThreeComponentParallel:
 
 class ThreeComponentSequential:
     model = read_model_from_yaml(MODEL_3C_SEQUENTIAL)
-    sim_model = read_model_from_yaml(MODEL_SIM_3C_SEQUENTIAL)
     initial_parameters = read_parameters_from_yaml(PARAMETERS_3C_INITIAL_SEQUENTIAL)
     wanted_parameters = read_parameters_from_yaml(PARAMETERS_3C_SIM_SEQUENTIAL)
     time = np.asarray(np.arange(-10, 50, 1.0))
@@ -294,14 +249,10 @@ def test_kinetic_model(suite, nnls):
     print(model.validate())
     assert model.valid()
 
-    sim_model = suite.sim_model
-    print(sim_model.validate())
-    assert sim_model.valid()
-
     wanted_parameters = suite.wanted_parameters
-    print(sim_model.validate(wanted_parameters))
+    print(model.validate(wanted_parameters))
     print(wanted_parameters)
-    assert sim_model.valid(wanted_parameters)
+    assert model.valid(wanted_parameters)
 
     initial_parameters = suite.initial_parameters
     print(model.validate(initial_parameters))
@@ -309,7 +260,7 @@ def test_kinetic_model(suite, nnls):
 
     print(model.markdown(wanted_parameters))
 
-    dataset = sim_model.simulate("dataset1", wanted_parameters, suite.axis)
+    dataset = model.simulate("dataset1", wanted_parameters, suite.axis)
 
     assert dataset.data.shape == (suite.axis["time"].size, suite.axis["spectral"].size)
 
@@ -340,10 +291,3 @@ def test_kinetic_model(suite, nnls):
 
     assert "species_associated_spectra" in resultdata
     assert "decay_associated_spectra" in resultdata
-
-
-if __name__ == "__main__":
-    test_kinetic_model(ThreeComponentParallel(), True)
-    test_kinetic_model(ThreeComponentParallel(), False)
-    test_kinetic_model(ThreeComponentSequential(), True)
-    test_kinetic_model(ThreeComponentSequential(), False)
