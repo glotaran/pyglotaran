806661f819baf9f8f51efedbec0d5a78ab2574bf
s-weigand <s.weigand.phy@gmail.com>
2021-02-28 19:09:26 +0100

    ⬆️ Dump version from 0.3.1 -> 0.3.2
    Since there was a problem with the 'setup.cfg' resulting in an inproperly rendered release page for the 0.3.1 on PyPi we need to do a 0.3.2, which besides the fixed metadata in 'setup.cfg' is the same as 0.3.1.

--- glotaran/__init__.py
+++ glotaran/__init__.py
@@ -5,5 +5,5 @@ from importlib import metadata
 for entry_point in metadata.entry_points()["glotaran.plugins"]:
     entry_point.load()
 
-__version__ = "0.3.0"
+__version__ = "0.3.2"
 # TODO: add git SHA1 information
