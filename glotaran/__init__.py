ee1e386547cf269387686e14bfba7462aec80ec0
s-weigand <s.weigand.phy@gmail.com>
2021-03-07 15:56:22 +0100

    Splitup entry points to be more granular

--- glotaran/__init__.py
+++ glotaran/__init__.py
@@ -2,8 +2,10 @@
 
 from importlib import metadata
 
-for entry_point in metadata.entry_points()["glotaran.plugins"]:
-    entry_point.load()
+for entry_point_name, entry_points in metadata.entry_points().items():
+    if entry_point_name.startswith("glotaran.plugins"):
+        for entry_point in entry_points:
+            entry_point.load()
 
 __version__ = "0.3.2"
 # TODO: add git SHA1 information
