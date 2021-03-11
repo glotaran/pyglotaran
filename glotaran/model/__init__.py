630622ee905f4988322cdb95ff554676524c44a5
s-weigand <s.weigand.phy@gmail.com>
2021-03-11 22:38:10 +0100

    ♻️Reimplemented model registration functions with new plugin system

--- glotaran/model/__init__.py
+++ glotaran/model/__init__.py
@@ -4,12 +4,13 @@ This package contains the Glotaran's base model object, the model decorators and
 common model items.
 """
 
+from glotaran.plugin_system.model_registration import get_model
+from glotaran.plugin_system.model_registration import known_model
+from glotaran.plugin_system.model_registration import known_model_names
+
 from .attribute import model_attribute
 from .attribute import model_attribute_typed
 from .base_model import Model
 from .dataset_descriptor import DatasetDescriptor
 from .decorator import model
-from .register import get_model
-from .register import known_model
-from .register import known_model_names
 from .weight import Weight
--- glotaran/model/test/test_register.py
+++
@@ -1,7 +0,0 @@
-from glotaran import model
-from glotaran.model.test.test_model import MockModel
-
-
-def test_register():
-    assert model.known_model("mock_model")
-    assert issubclass(model.get_model("mock_model"), MockModel)
