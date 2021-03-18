b9e024606f5672c7410e61a938c936c0b75b2480
s-weigand <s.weigand.phy@gmail.com>
2021-03-18 02:30:25 +0100

    🧹 Directly import 'load_plugins' from its definition

--- glotaran/__init__.py
+++ glotaran/__init__.py
@@ -1,6 +1,6 @@
 """Glotaran package __init__.py"""
 
-from glotaran.plugin_system import load_plugins
+from glotaran.plugin_system.base_registry import load_plugins
 
 load_plugins()
 
--- glotaran/plugin_system/__init__.py
+++ glotaran/plugin_system/__init__.py
@@ -1,3 +1,1 @@
 """Plugin system package containing all plugin related implementations."""
-
-from glotaran.plugin_system.base_registry import load_plugins
