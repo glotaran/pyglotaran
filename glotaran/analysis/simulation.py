🩹 Fix calculation of skewed-gaussian in spectral model (#686)
* 🩹 Fix calculation of 'skewed-gaussian'

Co-authored-by: jsnel  <jsnel@users.noreply.github.com>

The implementations are based on  TIMP:

https://github.com/glotaran/TIMP/blob/880ad8b53146403d8cffd9e230de1fb93db36e30/pkg/R/skew.R
https://github.com/glotaran/TIMP/blob/880ad8b53146403d8cffd9e230de1fb93db36e30/pkg/R/calcEhiergaus.R

* 📚 Added docstrings with equations for the implementations

* 🔧 Raised interrogate minimum value to 52% (current value 53.5%)

* ♻️ Renamed 'SpectralShapeLorentzianBandshape' to 'SpectralShapeLorentzian'

And the model attribute from 'lorentzian-bandshape' to 'lorentzian'
following https://github.com/glotaran/pyglotaran/pull/686#pullrequestreview-676898999

* Updated skewed-gaussian spectral model

Changed 'lorentzian' to 'skewed-gaussian', renamed classes and model_attribute_typed accordingly.
Renamed model_attribute `skew` to `skewness`
Change parameter \sigma to \Delta to better reflect it represents a width (FWHM) and not the \sigma parameter commonly associated with a Gaussian

Updated test_spectral_model with appropriate test values.

* Address xarray deprecation warnings

Fixed DeprecationWarning: Using a DataArray object to construct a variable is ambiguous, please extract the data using the .data property. This will raise a TypeError in 0.19.0.

Co-authored-by: Joris Snellenburg <jsnel@users.noreply.github.com>

glotaran/analysis/simulation.py
@@ -55,10 +55,10 @@ def simulate(
     model_dimension = axes[model.model_dimension]
     global_dimension = axes[model.global_dimension]
 
-    dim1 = model_dimension.size
+    # dim1 = model_dimension.size
     dim2 = global_dimension.size
     result = xr.DataArray(
-        np.empty((dim1, dim2), dtype=np.float64),
+        data=0.0,
         coords=[
             (model.model_dimension, model_dimension),
             (model.global_dimension, global_dimension),

glotaran/builtin/models/kinetic_spectrum/spectral_shape.py
@@ -24,7 +24,7 @@ class SpectralShapeGaussian:
         Parameters
         ----------
         axis: np.ndarray
-            The axies to calculate the shape on.
+            The axis to calculate the shape on.
 
         Returns
         -------

glotaran/builtin/models/spectral/shape.py
@@ -12,49 +12,154 @@ from glotaran.parameter import Parameter
         "amplitude": Parameter,
         "location": Parameter,
         "width": Parameter,
-        "skew": {"type": Parameter, "allow_none": True},
+        "skewness": {"type": Parameter, "allow_none": True},
     },
     has_type=True,
 )
-class SpectralShapeLorentzianBandshape:
-    """A gaussian spectral shape"""
+class SpectralShapeSkewedGaussian:
+    """A (skewed) Gaussian spectral shape"""
 
     def calculate(self, axis: np.ndarray) -> np.ndarray:
-        """calculate calculates the shape.
+        r"""Calculate a (skewed) Gaussian shape for a given ``axis``.
+
+        If a non-zero ``skewness`` parameter was added
+        :func:`calculate_skewed_gaussian` will be used.
+        Otherwise it will use :func:`calculate_gaussian`.
 
         Parameters
         ----------
         axis: np.ndarray
-            The axies to calculate the shape on.
+            The axis to calculate the shape for.
 
         Returns
         -------
         shape: numpy.ndarray
+            A Gaussian shape.
+
+        See Also
+        --------
+        calculate_gaussian
+        calculate_skewed_gaussian
+
+        Note
+        ----
+        Internally ``axis`` is converted from :math:`\mbox{nm}` to
+        :math:`1/\mbox{cm}`, thus ``location`` and ``width`` also need to
+        be provided in :math:`1/\mbox{cm}` (``1e7/value_in_nm``).
 
         """
         return (
-            self._calculate_skewed(axis)
-            if self.skew is not None
-            else self._calculate_unskewed(axis)
+            self.calculate_skewed_gaussian(axis)
+            if self.skewness is not None and not np.allclose(self.skewness, 0)
+            else self.calculate_gaussian(axis)
         )
 
-    def _calculate_unskewed(self, axis: np.ndarray) -> np.ndarray:
+    def calculate_gaussian(self, axis: np.ndarray) -> np.ndarray:
+        r"""Calculate a normal Gaussian shape for a given ``axis``.
+
+        The following equation is used for the calculation:
+
+        .. math::
+            f(x, A, x_0, \Delta) = A \exp \left({-
+            \frac{
+                \log{\left(2 \right)
+                \left(2(x - x_{0})\right)^{2}
+            }}{\Delta^{2}}}\right)
+
+        The parameters of the equation represent the following attributes of the shape:
+
+        - :math:`x` :       ``axis``
+
+        - :math:`A` :       ``amplitude``
+
+        - :math:`x_0` :     ``location``
+
+        - :math:`\Delta` :  ``width``
+
+        In this formalism, :math:`\Delta` represents the full width at half maximum (FWHM).
+        Compared to the more common definition
+        :math:`\exp \left(- (x-\mu )^{2}/(2\sigma^{2})\right)`
+        we have :math:`\sigma = \Delta/(2\sqrt{2\ln(2)})=\Delta/2.35482`
+
+        Parameters
+        ----------
+        axis : np.ndarray
+            The axis to calculate the shape for.
+
+        Returns
+        -------
+        np.ndarray
+            An array representing a Gaussian shape.
+        """
         return self.amplitude * np.exp(
-            -np.log(2) * np.square(2 * (axis - self.location) / self.width)
+            -np.log(2) * np.square(2 * ((1e7 / axis) - self.location) / self.width)
         )
 
-    def _calculate_skewed(self, axis: np.ndarray) -> np.ndarray:
-        return self.amplitude * np.exp(
-            -np.log(2)
-            * np.square(
-                np.log(1 + 2 * self.skew * (axis - self.location) / self.width) / self.skew
-            )
+    def calculate_skewed_gaussian(self, axis: np.ndarray) -> np.ndarray:
+        r"""Calculate the skewed Gaussian shape for ``axis``.
+
+        The following equation is used for the calculation:
+
+        .. math::
+            f(x, x_0, A, \Delta, b) =
+            \left\{
+                \begin{array}{ll}
+                    0                                               & \mbox{if } \theta \leq 0 \\
+                    A \exp \left({- \dfrac{\log{\left(2 \right)}
+                    \log{\left(\theta(x, x_0, \Delta, b) \right)}^{2}}{b^{2}}}\right)
+                                                                    & \mbox{if } \theta > 0
+                \end{array}
+            \right.
+
+        With:
+
+        .. math::
+            \theta(x, x_0, \Delta, b) = \frac{2 b \left(x - x_{0}\right) + \Delta}{\Delta}
+
+        The parameters of the equation represent the following attributes of the shape:
+
+        - :math:`x` :       ``axis``
+
+        - :math:`A` :       ``amplitude``
+
+        - :math:`x_0` :     ``location``
+
+        - :math:`\Delta` :  ``width``
+
+        - :math:`b` :       ``skewness``
+
+        Where :math:`\Delta` represents the full width at half maximum (FWHM),
+        see :func:`calculate_gaussian`.
+
+        Note that in the limit of skewness parameter :math:`b` equal to zero
+        :math:`f(x, x_0, A, \Delta, b)` simplifies to a normal gaussian
+        (since :math:`\lim_{b \to 0} \frac{\ln(1+bx)}{b}=x`),
+        see the definition in :func:`calculate_gaussian`.
+
+        Parameters
+        ----------
+        axis : np.ndarray
+            The axis to calculate the shape for.
+
+
+        Returns
+        -------
+        np.ndarray
+            An array representing a skewed Gaussian shape.
+        """
+        log_args = 1 + (2 * self.skewness * ((1e7 / axis) - self.location) / self.width)
+        result = np.zeros(log_args.shape)
+        valid_arg_mask = np.where(log_args > 0)
+        result[valid_arg_mask] = self.amplitude * np.exp(
+            -np.log(2) * np.square(np.log(log_args[valid_arg_mask]) / self.skewness)
         )
+
+        return result
 
 
 @model_attribute(properties={}, has_type=True)
 class SpectralShapeOne:
-    """A gaussian spectral shape"""
+    """A constant spectral shape with value 1"""
 
     def calculate(self, axis: np.ndarray) -> np.ndarray:
         """calculate calculates the shape.
@@ -62,7 +167,7 @@ class SpectralShapeOne:
         Parameters
         ----------
         axis: np.ndarray
-            The axies to calculate the shape on.
+            The axis to calculate the shape on.
 
         Returns
         -------
@@ -74,17 +179,17 @@ class SpectralShapeOne:
 
 @model_attribute(properties={}, has_type=True)
 class SpectralShapeZero:
-    """A gaussian spectral shape"""
+    """A constant spectral shape with value 0"""
 
     def calculate(self, axis: np.ndarray) -> np.ndarray:
         """calculate calculates the shape.
 
-        Only works after calling calling 'fill'.
+        Only works after calling ``fill``.
 
         Parameters
         ----------
         axis: np.ndarray
-            The axies to calculate the shape on.
+            The axis to calculate the shape on.
 
         Returns
         -------
@@ -96,7 +201,7 @@ class SpectralShapeZero:
 
 @model_attribute_typed(
     types={
-        "lorentzian-bandshape": SpectralShapeLorentzianBandshape,
+        "skewed-gaussian": SpectralShapeSkewedGaussian,
         "one": SpectralShapeOne,
         "zero": SpectralShapeZero,
     }