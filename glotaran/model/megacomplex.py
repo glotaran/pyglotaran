8f10ae2ef1fedb6cd6906bd54767b4b74bbfb3cb
Jörn Weißenborn <joern.weissenborn@gmail.com>
2021-06-25 13:27:30 +0200

    Added docstrings to megacomplex

--- glotaran/model/megacomplex.py
+++ glotaran/model/megacomplex.py
@@ -13,15 +13,28 @@ if TYPE_CHECKING:
 
 def megacomplex(
     dimension: str,
-    properties: Any | dict[str, dict[str, Any]] = {},
-    attributes: dict[str, dict[str, Any]] = {},
-    dataset_attributes: dict[str, dict[str, Any]] = {},
+    properties: Any | dict[str, dict[str, Any]] = None,
+    attributes: dict[str, dict[str, Any]] = None,
+    dataset_attributes: dict[str, dict[str, Any]] = None,
 ):
+    """The `@megacomplex` decorator is intended to be used on subclasses of
+    :class:`glotaran.model.Megacomplex`. It registers the megacomplex model
+    and makes it available in analysis models.
+    """
+
+    # TODO: this is temporary and will change in follow up PR
+    properties = properties if properties is not None else {}
     properties["dimension"] = {"type": str, "default": dimension}
     return model_attribute(properties=properties, has_type=True)
 
 
 class Megacomplex:
+    """A base class for megacomplex models.
+
+    Subclasses must overwrite :method:`glotaran.model.Megacomplex.calculate_matrix`
+    and :method:`glotaran.model.Megacomplex.index_dependent`.
+    """
+
     def calculate_matrix(
         self,
         model,
