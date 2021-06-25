fdea678a83902c8672181c04c5603f721ce43f64
s-weigand <s.weigand.phy@gmail.com>
2021-06-25 13:56:35 +0200

    🧪 Fixed deprecation tests for deprecated model properties

--- glotaran/deprecation/modules/test/test_model_base_model.py
+++ glotaran/deprecation/modules/test/test_model_base_model.py
@@ -1,6 +1,8 @@
 """Test deprecated functionality in 'glotaran.model.base_model'."""
 from __future__ import annotations
 
+import pytest
+
 from glotaran.deprecation.modules.test import deprecation_warning_on_call_test_helper
 from glotaran.model.base_model import Model
 from glotaran.model.test.test_model import MockModel
@@ -16,12 +18,13 @@ def test_model_index_dependent_method():
     deprecation_warning_on_call_test_helper(MockModel().index_dependent)
 
 
-def test_model_global_dimension_method():
+def test_model_global_dimension_property():
     """Model.global_dimension raises deperecation warning"""
-    deprecation_warning_on_call_test_helper(MockModel().global_dimension)
+    with pytest.warns(DeprecationWarning):
+        MockModel().global_dimension
 
 
-def test_model_model_dimension_method():
+def test_model_model_dimension_property():
     """Model.model_dimension raises deperecation warning"""
-    MockModel().model_dimension
-    deprecation_warning_on_call_test_helper(MockModel().model_dimension)
+    with pytest.warns(DeprecationWarning):
+        MockModel().model_dimension
