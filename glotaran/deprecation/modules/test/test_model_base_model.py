d555cac766315d6eb7786f1da4e7cb1653e31c1c
Jörn Weißenborn <joern.weissenborn@gmail.com>
2021-06-25 13:39:55 +0200

    Added deprecation tests

--- glotaran/deprecation/modules/test/test_model_base_model.py
+++ glotaran/deprecation/modules/test/test_model_base_model.py
@@ -3,8 +3,25 @@ from __future__ import annotations
 
 from glotaran.deprecation.modules.test import deprecation_warning_on_call_test_helper
 from glotaran.model.base_model import Model
+from glotaran.model.test.test_model import MockModel
 
 
 def test_model_simulate_method():
     """Model.simulate raises deperecation warning"""
     deprecation_warning_on_call_test_helper(Model().simulate)
+
+
+def test_model_index_dependent_method():
+    """Model.index_dependent raises deperecation warning"""
+    deprecation_warning_on_call_test_helper(MockModel().index_dependent)
+
+
+def test_model_global_dimension_method():
+    """Model.global_dimension raises deperecation warning"""
+    deprecation_warning_on_call_test_helper(MockModel().global_dimension)
+
+
+def test_model_model_dimension_method():
+    """Model.model_dimension raises deperecation warning"""
+    MockModel().model_dimension
+    deprecation_warning_on_call_test_helper(MockModel().model_dimension)
