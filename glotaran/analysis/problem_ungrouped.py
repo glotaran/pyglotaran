Correct hard coded label in ungrouped problem calculation
label instead of "dataset1"
Also included some auto refactoring from Sourcery

glotaran/analysis/problem_ungrouped.py
@@ -172,8 +172,8 @@ class UngroupedProblem(Problem):
             self._additional_penalty.append(additional_penalty)
 
     def _calculate_full_model_residual(self, label: str, dataset_model: DatasetModel):
-        model_matrix = self.matrices["dataset1"].matrix
-        global_matrix = self.global_matrices["dataset1"].matrix
+        model_matrix = self.matrices[label].matrix
+        global_matrix = self.global_matrices[label].matrix
 
         matrix = np.kron(global_matrix, model_matrix)
         weight = self._flattened_weights.get(label)

glotaran/analysis/test/test_problem.py
@@ -215,4 +215,4 @@ def test_full_model_problem():
 
     assert clp.shape == (4, 4)
     print(np.diagonal(clp))
-    assert all([np.isclose(1.0, c) for c in np.diagonal(clp)])
+    assert all(np.isclose(1.0, c) for c in np.diagonal(clp))