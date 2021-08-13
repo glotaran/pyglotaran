b92fff4f6fa4d6753d80d97c0f13f01a56c8f0ba
Jörn Weißenborn <joern.weissenborn@gmail.com>
2021-08-13 15:31:02 +0200

    Example model is now generated

--- glotaran/project/generators/test/test_genenerate_decay_model.py
+++ glotaran/project/generators/test/test_genenerate_decay_model.py
@@ -1,3 +1,4 @@
+import pytest
 from yaml import dump
 
 from glotaran.io import load_model
@@ -5,14 +6,12 @@ from glotaran.project.generators.generator import generate_parallel_model
 from glotaran.project.generators.generator import generate_sequential_model
 
 
-def test_generate_parallel_model(tmpdir_factory):
+def test_generate_parallel_model():
     nr_species = 5
     model_yaml = dump(generate_parallel_model(nr_species))
     print(model_yaml)  # noqa T001
-    model_file = tmpdir_factory.mktemp("gen_par_model") / "model.yml"
-    model_file.write_text(model_yaml, encoding="utf-8")
 
-    model = load_model(model_file)
+    model = load_model(model_yaml, format_name="yml_str")
 
     assert model.valid()
 
@@ -37,14 +36,13 @@ def test_generate_parallel_model(tmpdir_factory):
     assert dataset.megacomplex == ["megacomplex_parallel_decay"]
 
 
-def test_generate_decay_model(tmpdir_factory):
+@pytest.mark.parametrize("irf", [True, False])
+def test_generate_decay_model(irf):
     nr_species = 5
-    model_yaml = dump(generate_sequential_model(nr_species))
+    model_yaml = dump(generate_sequential_model(nr_species, irf=irf))
     print(model_yaml)  # noqa T001
-    model_file = tmpdir_factory.mktemp("gen_seq_model") / "model.yml"
-    model_file.write_text(model_yaml, encoding="utf-8")
 
-    model = load_model(model_file)
+    model = load_model(model_yaml, format_name="yml_str")
 
     print(model.validate())  # noqa T001
     assert model.valid()
@@ -52,10 +50,9 @@ def test_generate_decay_model(tmpdir_factory):
     assert "initial_concentration_dataset_1" in model.initial_concentration
     initial_concentration = model.initial_concentration["initial_concentration_dataset_1"]
     assert initial_concentration.compartments == [f"species_{i}" for i in range(nr_species)]
-    for i in range(nr_species):
-        assert (
-            initial_concentration.parameters[i].full_label == f"intitial_concentration.species_{i}"
-        )
+    assert initial_concentration.parameters[0].full_label == "initial_concentration.1"
+    for i in range(1, nr_species):
+        assert initial_concentration.parameters[i].full_label == "initial_concentration.0"
 
     assert "k_matrix_sequential" in model.k_matrix
     k_matrix = model.k_matrix["k_matrix_sequential"]
@@ -70,3 +67,10 @@ def test_generate_decay_model(tmpdir_factory):
     dataset = model.dataset["dataset_1"]
     assert dataset.initial_concentration == "initial_concentration_dataset_1"
     assert dataset.megacomplex == ["megacomplex_parallel_decay"]
+
+    if irf:
+        assert dataset.irf == "gaussian_irf"
+        assert "gaussian_irf" in model.irf
+        irf = model.irf["gaussian_irf"]
+        assert irf.center.full_label == "irf.center"
+        assert irf.width.full_label == "irf.width"
