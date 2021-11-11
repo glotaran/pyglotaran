89d8ded72c89200d1cad5b8859a6483f103082b5
Patrick <patrick.haetti@gmail.com>
2021-11-11 12:57:42 +0100

    updated path in test_pandas_parameters.py

--- glotaran/builtin/io/pandas/test/test_pandas_parameters.py
+++ glotaran/builtin/io/pandas/test/test_pandas_parameters.py
@@ -5,16 +5,15 @@ from pathlib import Path
 from glotaran.io import load_parameters
 from glotaran.io import save_parameters
 
-DATA_PATH1 = "data/parameter.yaml"
-DATA_PATH2 = "data/parameter.xlsx"
-DATA_PATH3 = "testparameters.xlsx"
+PATH_XLSX = Path.cwd().joinpath("glotaran/builtin/io/pandas/test/data/parameter.xlsx")
+PATH_YAML = Path.cwd().joinpath("glotaran/builtin/io/pandas/test/data/parameter.yaml")
+DATA_PATH = Path("testparameters.xlsx")
 
 
 def test_load_parameters_xlsx():
     """load parameters file as yaml and excel and compare them"""
-    parameters_xlsx = load_parameters(DATA_PATH2)
-    parameters_yaml = load_parameters(DATA_PATH1)
-
+    parameters_xlsx = load_parameters(PATH_XLSX)
+    parameters_yaml = load_parameters(PATH_YAML)
     assert parameters_yaml == parameters_xlsx
 
 
@@ -23,12 +22,10 @@ def test_save_parameters_xlsx(
 ):
     """load parameters file from yaml and save as xlsx
     and compare with yaml and reloaded xlsx parameter files"""
-
-    parameters_yaml = load_parameters(DATA_PATH1)
-    parameter_path = tmp_path / DATA_PATH3
+    parameters_yaml = load_parameters(PATH_YAML)
+    parameter_path = tmp_path / DATA_PATH
     save_parameters(file_name=parameter_path, format_name="xlsx", parameters=parameters_yaml)
     parameters_saved_and_reloaded = load_parameters(parameter_path)
-    parameters_prepared = load_parameters(DATA_PATH2)
-
+    parameters_xlsx = load_parameters(PATH_XLSX)
     assert parameters_yaml == parameters_saved_and_reloaded
-    assert parameters_prepared == parameters_saved_and_reloaded
+    assert parameters_xlsx == parameters_saved_and_reloaded
