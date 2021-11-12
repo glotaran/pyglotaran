788e26056cc3fc2281947d899d799623c2d744c9
Patrick <patrick.haetti@gmail.com>
2021-11-12 15:40:31 +0100

    Update test_pandas_parameters
    Added file 'parameter_diff_param.yaml' with different parameter types.
    Added 2 functions in 'test_pandas_parameters.py' for testing new parameter file.

--- glotaran/builtin/io/pandas/test/test_pandas_parameters.py
+++ glotaran/builtin/io/pandas/test/test_pandas_parameters.py
@@ -8,6 +8,7 @@ from glotaran.io import save_parameters
 PANDAS_TEST_DATA = Path(__file__).parent / "data"
 PATH_XLSX = PANDAS_TEST_DATA / "parameter.xlsx"
 PATH_YAML = PANDAS_TEST_DATA / "parameter.yaml"
+PATH_YAML_DIFF_PARAM = PANDAS_TEST_DATA / "parameter_diff_param.yaml"
 
 
 def test_load_parameters_xlsx():
@@ -29,3 +30,30 @@ def test_save_parameters_xlsx(
     parameters_xlsx = load_parameters(PATH_XLSX)
     assert parameters_yaml == parameters_saved_and_reloaded
     assert parameters_xlsx == parameters_saved_and_reloaded
+
+
+def test_save_parameters_xlsx_with_different_parameter_types(
+    tmp_path: Path,
+):
+    """load yaml parameter file which has different parameter types,
+    then save parameters in xlsx file.
+    load xlsx file and compare with yaml parameter file"""
+    parameters_yaml_diff_param = load_parameters(PATH_YAML_DIFF_PARAM)
+    parameter_path = tmp_path / "testparameters.xlsx"
+    save_parameters(
+        file_name=parameter_path, format_name="xlsx", parameters=parameters_yaml_diff_param
+    )
+    parameters_saved_and_reloaded = load_parameters(parameter_path)
+    parameters_xlsx = load_parameters(PATH_XLSX)
+    assert parameters_yaml_diff_param == parameters_saved_and_reloaded
+    assert parameters_xlsx == parameters_saved_and_reloaded
+
+
+def test_load_parameters_xlsx_with_different_parameter_types():
+    """load parameter file as yaml and excel and compare them
+    also load parameter file with different parameter types and compare with other"""
+    parameters_xlsx = load_parameters(PATH_XLSX)
+    parameters_yaml = load_parameters(PATH_YAML)
+    parameters_yaml_diff_param = load_parameters(PATH_YAML_DIFF_PARAM)
+    assert parameters_yaml == parameters_yaml_diff_param
+    assert parameters_yaml == parameters_xlsx
