ca868786408b65cfe001aecbde65dcd55fdeae3c
Patrick <patrick.haetti@gmail.com>
2021-11-12 12:48:20 +0100

    Updated file locations.
    Now relating to the location of the current file.

--- glotaran/builtin/io/pandas/test/test_pandas_parameters.py
+++ glotaran/builtin/io/pandas/test/test_pandas_parameters.py
@@ -5,9 +5,9 @@ from pathlib import Path
 from glotaran.io import load_parameters
 from glotaran.io import save_parameters
 
-PATH_XLSX = Path.cwd().joinpath("glotaran/builtin/io/pandas/test/data/parameter.xlsx")
-PATH_YAML = Path.cwd().joinpath("glotaran/builtin/io/pandas/test/data/parameter.yaml")
-DATA_PATH = Path("testparameters.xlsx")
+PANDAS_TEST_DATA = Path(__file__).parent / "data"
+PATH_XLSX = PANDAS_TEST_DATA / "parameter.xlsx"
+PATH_YAML = PANDAS_TEST_DATA / "parameter.yaml"
 
 
 def test_load_parameters_xlsx():
@@ -23,7 +23,7 @@ def test_save_parameters_xlsx(
     """load parameters file from yaml and save as xlsx
     and compare with yaml and reloaded xlsx parameter files"""
     parameters_yaml = load_parameters(PATH_YAML)
-    parameter_path = tmp_path / DATA_PATH
+    parameter_path = tmp_path / "testparameters.xlsx"
     save_parameters(file_name=parameter_path, format_name="xlsx", parameters=parameters_yaml)
     parameters_saved_and_reloaded = load_parameters(parameter_path)
     parameters_xlsx = load_parameters(PATH_XLSX)
