ae287566815d12ee2dfbd24daf440a4d829e26e8
Patrick <patrick.haetti@gmail.com>
2021-11-22 12:10:43 +0100

    csv parameter inf/-inf: convert minimum/maximum columns
    save/load csv parameter:  replace inf / -inf with NaN when saving and replace NaN with inf in 'maximum' and -inf in 'minimum ' columns of Dataframe.
    added 'import numpy'

--- glotaran/builtin/io/pandas/csv.py
+++ glotaran/builtin/io/pandas/csv.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import numpy as np
 import pandas as pd
 
 from glotaran.io import ProjectIoInterface
@@ -11,8 +12,13 @@ from glotaran.parameter import ParameterGroup
 class CsvProjectIo(ProjectIoInterface):
     def load_parameters(self, file_name: str) -> ParameterGroup:
         df = pd.read_csv(file_name, skipinitialspace=True, na_values=["None", "none"])
+        df["minimum"].fillna(-np.inf, inplace=True)
+        df["maximum"].fillna(np.inf, inplace=True)
         return ParameterGroup.from_dataframe(df, source=file_name)
 
     def save_parameters(self, parameters: ParameterGroup, file_name: str, *, sep=","):
         """Save a :class:`ParameterGroup` to a CSV file."""
-        parameters.to_dataframe().to_csv(file_name, na_rep="None", index=False, sep=sep)
+        df = parameters.to_dataframe()
+        df["minimum"].replace([-np.inf], "", inplace=True)
+        df["maximum"].replace([np.inf], "", inplace=True)
+        df.to_csv(file_name, na_rep="None", index=False, sep=sep)
