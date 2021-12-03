93d00cbaf2d8b0c1b4dd1caf7c70da80960f2184
patrickhaetti <patrick.haetti@gmail.com>
2021-12-03 12:39:34 +0100

    Take out additional parameter file
    Take out 'parameter_diff_param.yaml' from builtin/io/pandas/test/ which had varied values of 'parameter.yaml'.
    Taken out test functions from 'test_pandas_parameters.py' which were based on  'parameter_diff_param.yaml'.

--- glotaran/builtin/io/pandas/test/test_pandas_parameters.py
+++ glotaran/builtin/io/pandas/test/test_pandas_parameters.py
@@ -10,7 +10,6 @@ PATH_XLSX = PANDAS_TEST_DATA / "parameter.xlsx"
 PATH_CSV = PANDAS_TEST_DATA / "parameter.csv"
 PATH_TSV = PANDAS_TEST_DATA / "parameter.tsv"
 PATH_YAML = PANDAS_TEST_DATA / "parameter.yaml"
-PATH_YAML_DIFF_PARAM = PANDAS_TEST_DATA / "parameter_diff_param.yaml"
 
 
 def test_load_parameters_xlsx():
@@ -34,33 +33,6 @@ def test_save_parameters_xlsx(
     assert parameters_xlsx == parameters_saved_and_reloaded
 
 
-def test_save_parameters_xlsx_with_different_parameter_types(
-    tmp_path: Path,
-):
-    """load yaml parameter file which has different parameter types,
-    then save parameters in xlsx file.
-    load xlsx file and compare with yaml parameter file"""
-    parameters_yaml_diff_param = load_parameters(PATH_YAML_DIFF_PARAM)
-    parameter_path = tmp_path / "testparameters.xlsx"
-    save_parameters(
-        file_name=parameter_path, format_name="xlsx", parameters=parameters_yaml_diff_param
-    )
-    parameters_saved_and_reloaded = load_parameters(parameter_path)
-    parameters_xlsx = load_parameters(PATH_XLSX)
-    assert parameters_yaml_diff_param == parameters_saved_and_reloaded
-    assert parameters_xlsx == parameters_saved_and_reloaded
-
-
-def test_load_parameters_xlsx_with_different_parameter_types():
-    """load parameter file as yaml and excel and compare them
-    also load parameter file with different parameter types and compare with other"""
-    parameters_xlsx = load_parameters(PATH_XLSX)
-    parameters_yaml = load_parameters(PATH_YAML)
-    parameters_yaml_diff_param = load_parameters(PATH_YAML_DIFF_PARAM)
-    assert parameters_yaml == parameters_yaml_diff_param
-    assert parameters_yaml == parameters_xlsx
-
-
 def test_load_parameters_csv():
     """load parameters file as yaml and csv and compare them"""
     parameters_csv = load_parameters(PATH_CSV)
@@ -82,33 +54,6 @@ def test_save_parameters_csv(
     assert parameters_csv == parameters_saved_and_reloaded
 
 
-def test_save_parameters_csv_with_different_parameter_types(
-    tmp_path: Path,
-):
-    """load yaml parameter file which has different parameter types,
-    then save parameters in csv file.
-    load csv file and compare with yaml parameter file"""
-    parameters_yaml_diff_param = load_parameters(PATH_YAML_DIFF_PARAM)
-    parameter_path = tmp_path / "testparameters.csv"
-    save_parameters(
-        file_name=parameter_path, format_name="csv", parameters=parameters_yaml_diff_param
-    )
-    parameters_saved_and_reloaded = load_parameters(parameter_path)
-    parameters_csv = load_parameters(PATH_CSV)
-    assert parameters_yaml_diff_param == parameters_saved_and_reloaded
-    assert parameters_csv == parameters_saved_and_reloaded
-
-
-def test_load_parameters_csv_with_different_parameter_types():
-    """load parameter file as yaml and csv and compare them
-    also load parameter file with different parameter types and compare with other"""
-    parameters_csv = load_parameters(PATH_CSV)
-    parameters_yaml = load_parameters(PATH_YAML)
-    parameters_yaml_diff_param = load_parameters(PATH_YAML_DIFF_PARAM)
-    assert parameters_yaml == parameters_yaml_diff_param
-    assert parameters_yaml == parameters_csv
-
-
 def test_load_parameters_tsv():
     """load parameters file as yaml and tsv and compare them"""
     parameters_tsv = load_parameters(PATH_TSV)
@@ -128,30 +73,3 @@ def test_save_parameters_tsv(
     parameters_tsv = load_parameters(PATH_TSV)
     assert parameters_yaml == parameters_saved_and_reloaded
     assert parameters_tsv == parameters_saved_and_reloaded
-
-
-def test_save_parameters_tsv_with_different_parameter_types(
-    tmp_path: Path,
-):
-    """load yaml parameter file which has different parameter types,
-    then save parameters in tsv file.
-    load tsv file and compare with yaml parameter file"""
-    parameters_yaml_diff_param = load_parameters(PATH_YAML_DIFF_PARAM)
-    parameter_path = tmp_path / "testparameters.tsv"
-    save_parameters(
-        file_name=parameter_path, format_name="tsv", parameters=parameters_yaml_diff_param
-    )
-    parameters_saved_and_reloaded = load_parameters(parameter_path)
-    parameters_tsv = load_parameters(PATH_TSV)
-    assert parameters_yaml_diff_param == parameters_saved_and_reloaded
-    assert parameters_tsv == parameters_saved_and_reloaded
-
-
-def test_load_parameters_tsv_with_different_parameter_types():
-    """load parameter file as yaml and tsv and compare them
-    also load parameter file with different parameter types and compare with other"""
-    parameters_tsv = load_parameters(PATH_TSV)
-    parameters_yaml = load_parameters(PATH_YAML)
-    parameters_yaml_diff_param = load_parameters(PATH_YAML_DIFF_PARAM)
-    assert parameters_yaml == parameters_yaml_diff_param
-    assert parameters_yaml == parameters_tsv
