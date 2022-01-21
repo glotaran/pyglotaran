72d6b21ba66ae2e7fee6ec4a232df58b9a929917
s-weigand <s.weigand.phy@gmail.com>
2022-01-21 01:29:11 +0100

    ✨ Added module_load_overwrite arg to deprecation functions for modules
    This allow showinging the correct new usage while importing the functionality from somewhere else.

--- glotaran/deprecation/test/dummy_package/deprecated_module_attribute.py
+++ glotaran/deprecation/test/dummy_package/deprecated_module_attribute.py
@@ -13,4 +13,12 @@ def __getattr__(attribute_name: str):
             to_be_removed_in_version="0.6.0",
         )
 
+    if attribute_name == "foo_bar":
+        return deprecate_module_attribute(
+            deprecated_qual_name=("glotaran.deprecation.test.dummy_package.foo_bar"),
+            new_qual_name="glotaran.does_not._need_to_exists",
+            to_be_removed_in_version="0.6.0",
+            module_load_overwrite="glotaran.deprecation.deprecation_utils.parse_version",
+        )
+
     raise AttributeError(f"module {__name__} has no attribute {attribute_name}")
