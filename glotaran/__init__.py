8f9a656784a93083affe32d446f412d7fea3fed7
s-weigand <s.weigand.phy@gmail.com>
2022-01-21 01:30:23 +0100

    🚧🗑️ Properly deprecated  'glotaran.examples'

--- glotaran/__init__.py
+++ glotaran/__init__.py
@@ -1,4 +1,5 @@
 """Glotaran package __init__.py"""
+from glotaran.deprecation.deprecation_utils import deprecate_submodule
 from glotaran.deprecation.modules.glotaran_root import read_model_from_yaml
 from glotaran.deprecation.modules.glotaran_root import read_model_from_yaml_file
 from glotaran.deprecation.modules.glotaran_root import read_parameters_from_csv_file
@@ -9,6 +10,13 @@ from glotaran.plugin_system.base_registry import load_plugins
 load_plugins()
 
 __version__ = "0.6.0.dev0"
+
+examples = deprecate_submodule(
+    deprecated_module_name="glotaran.examples",
+    new_module_name="glotaran.testing.simulated_data",
+    to_be_removed_in_version="0.8.0",
+    module_load_overwrite="glotaran.deprecation.modules.examples",
+)
 
 
 def __getattr__(attribute_name: str):
