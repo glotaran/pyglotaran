c6b91c1167da56449c2d99eaf04bb8a60369d0b5
s-weigand <s.weigand.phy@gmail.com>
2022-03-31 19:46:28 +0200

    🩹 Fix wrong item order on linux

--- glotaran/project/project_registry.py
+++ glotaran/project/project_registry.py
@@ -62,7 +62,8 @@ class ProjectRegistry:
         dict[str, Path]
             The items of the registry.
         """
-        return {path.stem: path for path in self._directory.iterdir() if self.is_item(path)}
+        items = {path.stem: path for path in self._directory.iterdir() if self.is_item(path)}
+        return dict(sorted(items.items()))
 
     def is_item(self, path: Path) -> bool:
         """Check if the path contains an registry item.
