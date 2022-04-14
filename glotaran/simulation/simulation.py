d77a8bbf38e6de30efd3cd75dde9279209ab1650
Joern Weissenborn <joern.weissenborn@gmail.com>
2022-04-14 13:15:38 +0200

    Adapted simulation.

--- glotaran/simulation/simulation.py
+++ glotaran/simulation/simulation.py
@@ -7,7 +7,7 @@ import numpy as np
 import xarray as xr
 
 from glotaran.model import DatasetModel
-from glotaran.optimization.util import calculate_matrix
+from glotaran.optimization.matrix_provider import MatrixProvider
 
 if TYPE_CHECKING:
     from numpy.typing import ArrayLike
@@ -60,10 +60,15 @@ def simulate(
         Raised if dataset model has no global megacomplex and no clp are provided.
     """
     dataset_model = model.dataset[dataset].fill(model, parameters)  # type:ignore[attr-defined]
-    dataset_model.set_coordinates(coordinates)
+    model_dimension = dataset_model.get_model_dimension()
+    model_axis = coordinates[model_dimension]
+    global_dimension = next(dim for dim in coordinates if dim != model_dimension)
+    global_axis = coordinates[global_dimension]
 
     if dataset_model.has_global_model():
-        result = simulate_full_model(dataset_model)
+        result = simulate_full_model(
+            dataset_model, global_dimension, global_axis, model_dimension, model_axis
+        )
     elif clp is None:
         raise ValueError(
             f"Cannot simulate dataset '{dataset}'. "
@@ -71,8 +76,7 @@ def simulate(
         )
     else:
         result = simulate_from_clp(
-            dataset_model,
-            clp,
+            dataset_model, global_dimension, global_axis, model_dimension, model_axis, clp
         )
 
     if noise:
@@ -83,13 +87,28 @@ def simulate(
     return result
 
 
-def simulate_from_clp(dataset_model: DatasetModel, clp: xr.DataArray) -> xr.Dataset:
+def simulate_from_clp(
+    dataset_model: DatasetModel,
+    global_dimension: str,
+    global_axis: ArrayLike,
+    model_dimension: str,
+    model_axis: ArrayLike,
+    clp: xr.DataArray,
+) -> xr.Dataset:
     """Simulate a dataset model from pre-defined conditionally linear parameters.
 
     Parameters
     ----------
     dataset_model : DatasetModel
         The dataset model to simulate.
+    global_dimension : str
+        The global dimension of the dataset.
+    global_axis : ArrayLike
+        The global axis of the dataset.
+    model_dimension : str
+        The model dimension of the dataset.
+    model_axis : ArrayLike
+        The model axis of the dataset.
     clp : xr.DataArray
         A matrix with conditionally linear parameters.
 
@@ -105,48 +124,56 @@ def simulate_from_clp(dataset_model: DatasetModel, clp: xr.DataArray) -> xr.Data
     """
     if "clp_label" not in clp.coords:
         raise ValueError("Missing coordinate 'clp_label' in clp.")
-    global_dimension = next(dim for dim in clp.coords if dim != "clp_label")
-
-    global_axis = clp.coords[global_dimension]
     matrices = (
         [
-            calculate_matrix(
-                dataset_model,
-                {global_dimension: index},
-            )
+            MatrixProvider.calculate_dataset_matrix(dataset_model, index, global_axis, model_axis)
             for index, _ in enumerate(global_axis)
         ]
         if dataset_model.is_index_dependent()
-        else calculate_matrix(dataset_model, {})
+        else [
+            MatrixProvider.calculate_dataset_matrix(dataset_model, None, global_axis, model_axis)
+        ]
+        * global_axis.size
     )
 
-    model_dimension = dataset_model.get_model_dimension()
-    model_axis = dataset_model.get_coordinates()[model_dimension]
     result = xr.DataArray(
         data=0.0,
         coords=[
-            (model_dimension, model_axis.data),
-            (global_dimension, global_axis.data),
+            (model_dimension, model_axis),
+            (global_dimension, global_axis),
         ],
     )
     result = result.to_dataset(name="data")
     for i in range(global_axis.size):
-        index_matrix = matrices[i] if dataset_model.is_index_dependent() else matrices
         result.data[:, i] = np.dot(
-            index_matrix.matrix,
-            clp.isel({global_dimension: i}).sel({"clp_label": index_matrix.clp_labels}),
+            matrices[i].matrix,
+            clp.isel({global_dimension: i}).sel({"clp_label": matrices[i].clp_labels}),
         )
 
     return result
 
 
-def simulate_full_model(dataset_model: DatasetModel) -> xr.Dataset:
+def simulate_full_model(
+    dataset_model: DatasetModel,
+    global_dimension: str,
+    global_axis: ArrayLike,
+    model_dimension: str,
+    model_axis: ArrayLike,
+) -> xr.Dataset:
     """Simulate a dataset model with global megacomplexes.
 
     Parameters
     ----------
     dataset_model : DatasetModel
         The dataset model to simulate.
+    global_dimension : str
+        The global dimension of the dataset.
+    global_axis : ArrayLike
+        The global axis of the dataset.
+    model_dimension : str
+        The model dimension of the dataset.
+    model_axis : ArrayLike
+        The model axis of the dataset.
 
     Returns
     -------
@@ -164,14 +191,18 @@ def simulate_full_model(dataset_model: DatasetModel) -> xr.Dataset:
     ):
         raise ValueError("Index dependent models for global dimension are not supported.")
 
-    global_matrix = calculate_matrix(dataset_model, {}, as_global_model=True)
+    global_matrix = MatrixProvider.calculate_dataset_matrix(
+        dataset_model, None, global_axis, model_axis, as_global_model=True
+    )
     global_clp_labels = global_matrix.clp_labels
     global_matrix = xr.DataArray(
         global_matrix.matrix.T,
         coords=[
             ("clp_label", global_clp_labels),
-            (dataset_model.get_global_dimension(), dataset_model.get_global_axis()),
+            (global_dimension, global_axis),
         ],
     )
 
-    return simulate_from_clp(dataset_model, global_matrix)
+    return simulate_from_clp(
+        dataset_model, global_dimension, global_axis, model_dimension, model_axis, global_matrix
+    )
