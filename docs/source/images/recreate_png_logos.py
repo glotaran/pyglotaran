cf859e9fb4acf22f0de7689ff826d7f6591ce8dd
Sebastian Weigand <s.weigand.phy@gmail.com>
2022-06-05 20:32:43 +0200

    🎨 Add new logo (#1083)
    * ✨🎨 Added new logos
    
    * ✨🎨 Added new favicons based on the new logos
    
    * 🩹 Fixed mask alignement
    
    * 👌🎨 Changed used color to use nearly black and nearly white
    
    and font stroke color to the same color as font fill color
    
    * ✨ Added script to render svg files to pgn with different sizes
    
    * 📚🚧 Added change to changelog
    
    * 📚 Changed logo and favicon in docs
    
    * 👌🎨 Made background color gradient of dark theme icon darker
    
    🧹 Renamed logos and favicons to dark/light theme
    👌🎨 Changed dark and light colors to be the same as github dark theme for better contrast
    
    * 🎨 Added rendered png versions of the icons

---
+++ docs/source/images/recreate_png_logos.py
@@ -0,0 +1,38 @@
+"""Helper script to render svg logos to png.
+
+This script uses https://github.com/shakiba/svgexport
+
+``npm install svgexport -g``
+"""
+
+import subprocess
+from pathlib import Path
+
+image_folder = Path(__file__).parent
+output_folder = image_folder / "png"
+
+output_folder.mkdir(parents=True, exist_ok=True)
+
+
+def render_svg(glob_pattern: str, size: int) -> None:
+    """Render svg file tp png file with size ``size x size``.
+
+    Parameters
+    ----------
+    glob_pattern: str
+        Patter to find svg files.
+    size: int
+        Size of the resulting png image.
+    """
+
+    for logo in image_folder.glob(glob_pattern):
+        input_file = logo.resolve().as_posix()
+        output_file = (output_folder / f"{logo.stem}_{size}x{size}.png").resolve().as_posix()
+        subprocess.run(f"svgexport {input_file} {output_file} {size}:", shell=True)
+
+
+if __name__ == "__main__":
+    for size in (16, 24, 32, 48):
+        render_svg("pyglotaran_favicon*.svg", size)
+    for size in (64, 128, 256, 512):
+        render_svg("pyglotaran_logo*.svg", size)
