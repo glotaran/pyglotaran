7a0ad27c5e6e815a86b3f1e2b2e9d2dd197e3881
Joern Weissenborn <joern.weissenborn@gmail.com>
2022-09-22 12:27:21 +0200

    Added item fill.

--- glotaran/model_new/item.py
+++ glotaran/model_new/item.py
@@ -4,8 +4,10 @@ from inspect import getmro
 from types import NoneType
 from types import UnionType
 from typing import TYPE_CHECKING
+from typing import Callable
 from typing import ClassVar
 from typing import Generator
+from typing import Iterator
 from typing import TypeAlias
 from typing import TypeVar
 from typing import Union
@@ -28,11 +30,6 @@ from attrs import resolve_types
 
 from glotaran.parameter import Parameter
 from glotaran.parameter import ParameterGroup
-
-T = TypeVar("T")
-
-ParameterType: TypeAlias = Parameter | str
-ModelItemType: TypeAlias = T | str
 
 
 class ItemIssue:
@@ -61,67 +58,7 @@ class ParameterIssue(ItemIssue):
 
 
 class Item:
-    def get_issues(
-        self, *, model: Model, parameters: ParameterGroup | None = None
-    ) -> list[ItemIssue]:
-        issues = self.get_model_issues(model)
-        if parameters is not None:
-            issues += self.get_parameter_issues(parameters)
-        return issues
-
-    def get_model_issues(self, model: Model) -> list[ItemIssue]:
-        return [
-            ModelItemIssue(name, label)
-            for name, label in iterate_model_item_names_and_labels(self)
-            if label not in getattr(model, name)
-        ]
-
-    def get_parameter_issues(self, parameters: ParameterGroup) -> list[ItemIssue]:
-        return [
-            ParameterIssue(label)
-            for name, label in iterate_parameter_names_and_labels(self)
-            if not parameters.has(label)
-        ]
-
-
-def item(cls):
-    parent = getmro(cls)[1]
-    cls = define(kw_only=True, slots=False)(cls)
-    if parent is ModelItemTyped:
-        cls.__model_item_types__ = {}
-    elif issubclass(cls, ModelItemTyped):
-        cls._register_item_class()
-    resolve_types(cls)
-    return cls
-
-
-@define(kw_only=True)
-class ModelItem(Item):
-    label: str
-
-
-@define(kw_only=True)
-class ModelItemTyped(ModelItem):
-    type: str
-    __model_item_types__: ClassVar[dict[str, type]]
-
-    @classmethod
-    def _register_item_class(cls):
-        item_type = cls.get_item_type()
-        if item_type is not NOTHING:
-            cls.__model_item_types__[item_type] = cls
-
-    @classmethod
-    def get_item_type(cls) -> str:
-        return fields(cls).type.default
-
-    @classmethod
-    def get_item_types(cls) -> str:
-        return cls.__model_item_types__.keys()
-
-    @classmethod
-    def get_item_type_class(cls, item_type: str) -> type:
-        return cls.__model_item_types__[item_type]
+    pass
 
 
 def iterate_attributes_of_type(item: Item, attr_type: type) -> Generator[Attribute, None, None]:
@@ -164,6 +101,117 @@ def iterate_model_item_names_and_labels(item: Item) -> Generator[tuple(str, str)
 
 def iterate_parameter_names_and_labels(item: Item) -> Generator[tuple(str, str), None, None]:
     return iterate_names_and_labels(item, parameter_attributes(item.__class__))
+
+
+def fill_item_attributes(
+    item: item, iterator: Iterator[Attribute], fill_function: Callable
+) -> list[ItemIssue]:
+    for attr in iterator:
+        value = getattr(item, attr.name)
+
+        structure, _ = strip_type_and_structure_from_attribute(attr)
+        if structure is dict:
+            value = {
+                k: fill_function(attr.name, v) if isinstance(v, str) else v
+                for k, v in value.items()
+            }
+        elif structure is list:
+            value = [fill_function(attr.name, v) if isinstance(v, str) else v for v in value]
+        else:
+            value = fill_function(attr.name, value) if isinstance(value, str) else value
+
+        setattr(item, attr.name, value)
+
+
+def fill_item(item: item, model: Model, parameters: ParameterGroup) -> Item:
+    fill_item_model_attributes(item, model, parameters)
+    fill_item_parameter_attributes(item, parameters)
+    return item
+
+
+def fill_item_model_attributes(item: item, model: Model, parameters: ParameterGroup):
+    fill_item_attributes(
+        item,
+        model_attributes(item.__class__),
+        lambda name, label: fill_item(getattr(model, name)[label], model, parameters),
+    )
+
+
+def fill_item_parameter_attributes(item: item, parameters: ParameterGroup) -> list[ItemIssue]:
+    fill_item_attributes(
+        item, parameter_attributes(item.__class__), lambda name, label: parameters.get(label)
+    )
+
+
+def get_item_model_issues(item: item, model: Model) -> list[ItemIssue]:
+    return [
+        ModelItemIssue(name, label)
+        for name, label in iterate_model_item_names_and_labels(item)
+        if label not in getattr(model, name)
+    ]
+
+
+def get_item_parameter_issues(item: item, parameters: ParameterGroup) -> list[ItemIssue]:
+    return [
+        ParameterIssue(label)
+        for name, label in iterate_parameter_names_and_labels(item)
+        if not parameters.has(label)
+    ]
+
+
+def get_item_issues(
+    *, item: Item, model: Model, parameters: ParameterGroup | None = None
+) -> list[ItemIssue]:
+    issues = get_item_model_issues(item, model)
+    if parameters is not None:
+        issues += get_item_parameter_issues(item, parameters)
+    return issues
+
+
+def item(cls):
+    parent = getmro(cls)[1]
+    cls = define(kw_only=True, slots=False)(cls)
+    if parent is ModelItemTyped:
+        cls.__model_item_types__ = {}
+    elif issubclass(cls, ModelItemTyped):
+        cls._register_item_class()
+    resolve_types(cls)
+    return cls
+
+
+@define(kw_only=True)
+class ModelItem(Item):
+    label: str
+
+
+T = TypeVar("ModelItemT", bound="ModelItem")
+
+ParameterType: TypeAlias = Parameter | str
+ModelItemType: TypeAlias = T | str
+
+
+@define(kw_only=True)
+class ModelItemTyped(ModelItem):
+    type: str
+    __model_item_types__: ClassVar[dict[str, type]]
+
+    @classmethod
+    def _register_item_class(cls):
+        item_type = cls.get_item_type()
+        if item_type is not NOTHING:
+            cls.__model_item_types__[item_type] = cls
+
+    @classmethod
+    def get_item_type(cls) -> str:
+        return fields(cls).type.default
+
+    @classmethod
+    def get_item_types(cls) -> str:
+        return cls.__model_item_types__.keys()
+
+    @classmethod
+    def get_item_type_class(cls, item_type: str) -> type:
+        return cls.__model_item_types__[item_type]
 
 
 def strip_type_and_structure_from_attribute(attr: Attribute) -> tuple[None | list | dict, type]:
