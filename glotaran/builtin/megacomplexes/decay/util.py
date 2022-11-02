fix bug in irf normalization (added an "s" after irfscale) (#1156)
Bug fix of issue filed under #1157 (originally reported by Ivo by e-mail Nov 1st, 2022)

* fix bug in irf normalization (added an "s" after irfscale)
* 📚 Add change to changelog

Co-authored-by: Ivo van Stokkum <ism200@users.noreply.github.com>
Co-authored-by: Joris Snellenburg <jsnel@users.noreply.github.com>

glotaran/builtin/megacomplexes/decay/util.py
@@ -163,7 +163,7 @@ def decay_matrix_implementation(
                 backsweep_period,
             )
         if dataset_model.irf.normalize:
-            matrix /= np.sum(irf_scale)
+            matrix /= np.sum(irf_scales)
 
     else:
         calculate_decay_matrix_no_irf(matrix, rates, model_axis)