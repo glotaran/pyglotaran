930af5bedb733d5fa8cf5a026fdd6e758a80dc63
Joern Weissenborn <joern.weissenborn@gmail.com>
2023-01-28 20:20:46 +0100

    Added OptimizationEstimation

--- glotaran/optimization/estimation.py
+++ glotaran/optimization/estimation.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import Literal
 
+import numpy as np
+
+from glotaran.model import ClpRelation
 from glotaran.optimization.nnls import residual_nnls
 from glotaran.optimization.variable_projection import residual_variable_projection
 from glotaran.typing.types import ArrayLike
@@ -12,13 +16,18 @@ SUPPORTED_RESIUDAL_FUNCTIONS = {
 }
 
 
-@dataclass(frozen=True)
+@dataclass
 class OptimizationEstimation:
     clp: ArrayLike
     residual: ArrayLike
 
     @classmethod
-    def calculate(cls, matrix: ArrayLike, data: ArrayLike) -> OptimizationEstimation:
+    def calculate(
+        cls,
+        matrix: ArrayLike,
+        data: ArrayLike,
+        residual_function: Literal["variable_projection", "non_negative_least_squares"],
+    ) -> OptimizationEstimation:
         """Calculate the clps and the residual for a matrix and data.
 
         Parameters
@@ -33,6 +42,29 @@ class OptimizationEstimation:
         tuple[np.typing.ArrayLike, np.typing.ArrayLike]
             The estimated clp and residual.
         """
+        residual_fn = SUPPORTED_RESIUDAL_FUNCTIONS[residual_function]
+        return cls(*residual_fn(matrix, data))
 
-        residual_function = SUPPORTED_RESIUDAL_FUNCTIONS[dataset_group.residual_function]
-        return cls(*residual_function(matrix, data))
+    def resolve_clp(
+        self,
+        clp_axis: list[str],
+        reduced_clp_axis: list[str],
+        index: float,
+        relations: list[ClpRelation],
+    ) -> OptimizationEstimation:
+        if len(relations) == 0:
+            return self
+
+        clps = np.zeros(len(clp_axis))
+        clps[[clp_axis.index(label) for label in reduced_clp_axis]]
+
+        for relation in relations:
+            if (
+                relation.target in clp_axis
+                and relation.source in clp_axis
+                and relation.applies(index)
+            ):
+                source_idx = clp_axis.index(relation.source)
+                target_idx = clp_axis.index(relation.target)
+                clps[target_idx] = relation.parameter * clps[source_idx]
+        return clps
