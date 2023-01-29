8aa333e2c0828f9a668177d39ae3d54d5c8c5a7d
Joern Weissenborn <joern.weissenborn@gmail.com>
2023-01-29 21:09:31 +0100

    Created Optimization

--- glotaran/optimization/objective.py
+++ glotaran/optimization/objective.py
@@ -2,8 +2,10 @@ import abc
 from typing import Literal
 
 import numpy as np
+import xarray as xr
 from numpy.typing import ArrayLike
 
+from glotaran.io.prepare_dataset import add_svd_to_dataset
 from glotaran.model import ClpConstraint
 from glotaran.model import ClpPenalty
 from glotaran.model import ClpRelation
@@ -39,6 +41,10 @@ class OptimizationObjective:
     def get_data(self, index: int) -> ArrayLike:
         pass
 
+    @abc.abstractmethod
+    def get_result(self) -> dict[str, xr.Dataset]:
+        pass
+
     def calculate(self) -> ArrayLike:
         matrices = self.calculate_matrices()
         reduced_matrices = [
@@ -47,7 +53,7 @@ class OptimizationObjective:
         ]
         estimations = [
             OptimizationEstimation.calculate(
-                matrices[i].array, self.get_data(i), self._residual_function
+                reduced_matrices[i].array, self.get_data(i), self._residual_function
             )
             for i in range(self._global_axis.size)
         ]
@@ -63,6 +69,55 @@ class OptimizationObjective:
             )
         return np.concatenate(penalties)
 
+    def get_result_dataset(self, data: OptimizationData) -> xr.Dataset:
+        dataset = data.model.data.copy()
+        dataset.attrs["model_dimension"] = data.model_dimension
+        dataset.attrs["global_dimension"] = data.global_dimension
+
+        matrix = OptimizationMatrix.from_data(data)
+        matrix_coords = (
+            (data.model_dimension, data.model_axis),
+            ("clp_label", matrix.clp_labels),
+        )
+        if matrix.is_index_dependent:
+            matrix_coords = ((data.global_dimension, data.global_axis),) + matrix_coords
+        dataset["matrix"] = xr.DataArray(matrix.array, coords=matrix_coords)
+
+        matrices = [matrix.at_index(i) for i in range(data.global_axis.size)]
+        reduced_matrices = [
+            matrices[i].reduce(index, self._constraints, self._relations)
+            for i, index in enumerate(data.global_axis)
+        ]
+        estimations = [
+            OptimizationEstimation.calculate(
+                reduced_matrices[i].array, self.get_data(i), self._residual_function
+            ).resolve_clp(
+                matrices[i].clp_labels,
+                reduced_matrices[i].clp_labels,
+                data.global_axis[i],
+                self._constraints,
+                self._relations,
+            )
+            for i in range(data.global_axis.size)
+        ]
+        dataset["clp"] = xr.DataArray(
+            [e.clp for e in estimations],
+            coords=((data.global_dimension, data.global_axis), ("clp_label", matrix.clp_labels)),
+        )
+        dataset["residual"] = xr.DataArray(
+            [e.residual for e in estimations],
+            coords=(
+                (data.global_dimension, data.global_axis),
+                (data.model_dimension, data.model_axis),
+            ),
+        )
+        # Calculate RMS
+        size = dataset.residual.shape[0] * dataset.residual.shape[1]
+        dataset.attrs["root_mean_square_error"] = np.sqrt(
+            (dataset.residual**2).sum() / size
+        ).data
+        return dataset
+
 
 class OptimizationObjectiveData(OptimizationObjective):
     def __init__(
@@ -71,8 +126,10 @@ class OptimizationObjectiveData(OptimizationObjective):
         constraints: list[ClpConstraint],
         relations: list[ClpRelation],
         penalties: list[ClpPenalty],
+        label: str = "dataset",
     ):
         self._data = OptimizationData(model)
+        self._label = label
         super.__init__(
             self._data.global_axis, constraints, relations, penalties, model.residual_function
         )
@@ -83,6 +140,9 @@ class OptimizationObjectiveData(OptimizationObjective):
 
     def get_data(self, index: int) -> ArrayLike:
         return self._data.data[:, index]
+
+    def get_result(self) -> dict[str, xr.Dataset]:
+        return {self.label: self.get_result_dataset(self._data)}
 
 
 class OptimizationObjectiveExperiment:
@@ -107,28 +167,7 @@ class OptimizationObjectiveExperiment:
     def get_data(self, index: int) -> ArrayLike:
         return self._data.data[index]
 
-    def calculate(self) -> ArrayLike:
-        matrices = OptimizationMatrix.from_linked_data(self._data)
-        reduced_matrices = [
-            matrices[i].reduce(index, self._constraints, self._relations)
-            for i, index in enumerate(self._data.global_axis)
-        ]
-        estimations = [
-            OptimizationEstimation.calculate(
-                matrices[i].array, self._data.data[:, i], self._data.model.residual_function
-            )
-            for i in range(self._data.global_axis.size)
-        ]
-
-        penalties = [e.residual for e in estimations]
-        if len(self._penalties) > 0:
-            estimations = [
-                e.resolve_clp(m.clp_labels, r.clp_labels, i, self._constraints, self._relations)
-                for e, m, r, i in zip(
-                    estimations, matrices, reduced_matrices, self._data.global_axis
-                )
-            ]
-            penalties += calculate_clp_penalties(
-                matrices, estimations, self._data.global_axis, self._penalties
-            )
-        return np.concatenate(penalties)
+    def get_result(self) -> dict[str, xr.Dataset]:
+        return {
+            label: self.get_result_dataset(data) for label, data in self._data.datasets.items()
+        }
---
+++ glotaran/optimization/result.py
@@ -0,0 +1,165 @@
+"""The result class for global analysis."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+import numpy as np
+from numpy.typing import ArrayLike
+from scipy.optimize import OptimizeResult
+
+from glotaran.optimization.optimization_history import OptimizationHistory
+from glotaran.parameter import ParameterHistory
+from glotaran.parameter import Parameters
+from glotaran.project.dataclass_helpers import exclude_from_dict_field
+from glotaran.project.dataclass_helpers import file_loadable_field
+
+
+@dataclass
+class OptimizationResult:
+    """The result of a global analysis."""
+
+    number_of_function_evaluations: int
+    """The number of function evaluations."""
+
+    success: bool
+    """Indicates if the optimization was successful."""
+
+    termination_reason: str
+    """The reason (message when) the optimizer terminated"""
+
+    glotaran_version: str
+    """The glotaran version used to create the result."""
+
+    free_parameter_labels: list[str]
+    """List of labels of the free parameters used in optimization."""
+
+    parameter_history: ParameterHistory = file_loadable_field(  # type:ignore[type-var]
+        ParameterHistory
+    )
+    """The parameter history."""
+
+    optimization_history: OptimizationHistory = file_loadable_field(  # type:ignore[type-var]
+        OptimizationHistory
+    )
+    """The optimization history."""
+
+    # The below can be none in case of unsuccessful optimization
+
+    cost: ArrayLike | None = exclude_from_dict_field(None)
+    """The final cost."""
+
+    chi_square: float | None = None
+    r"""The chi-square of the optimization.
+
+    :math:`\chi^2 = \sum_i^N [{Residual}_i]^2`."""
+
+    covariance_matrix: ArrayLike | None = exclude_from_dict_field(None)
+    """Covariance matrix.
+
+    The rows and columns are corresponding to :attr:`free_parameter_labels`."""
+
+    degrees_of_freedom: int | None = None
+    """Degrees of freedom in optimization :math:`N - N_{vars}`."""
+
+    jacobian: ArrayLike | list | None = exclude_from_dict_field(None)
+    """Modified Jacobian matrix at the solution
+
+    See also: :func:`scipy.optimize.least_squares`
+    """
+    number_of_data_points: int | None = None
+    """Number of data points :math:`N`."""
+    number_of_jacobian_evaluations: int | None = None
+    """The number of jacobian evaluations."""
+    number_of_parameters: int | None = None
+    """Number of parameters in optimization :math:`N_{vars}`"""
+    optimality: float | None = None
+    reduced_chi_square: float | None = None
+    r"""The reduced chi-square of the optimization.
+
+    :math:`\chi^2_{red}= {\chi^2} / {(N - N_{vars})}`.
+    """
+    root_mean_square_error: float | None = None
+    r"""
+    The root mean square error the optimization.
+
+    :math:`rms = \sqrt{\chi^2_{red}}`
+    """
+
+    @classmethod
+    def from_least_squares_result(
+        cls,
+        result: OptimizeResult | None,
+        parameter_history: ParameterHistory,
+        optimization_history: OptimizationHistory,
+        penalty: np.typing.ArrayLike,
+        free_parameter_labels: list[str],
+        termination_reason: str,
+    ):
+        success = result is not None
+
+        result_args = {
+            "success": success,
+            "glotaran_version": glotaran_version,
+            "free_parameter_labels": free_parameter_labels,
+            "parameter_history": parameter_history,
+            "termination_reason": termination_reason,
+            "optimization_history": optimization_history,
+            "number_of_function_evaluations": result.nfev
+            if success
+            else parameter_history.number_of_records,
+        }
+
+        result_args["cost"] = 0.5 * np.dot(penalty, penalty)
+        if success:
+            result_args["number_of_jacobian_evaluations"] = result.njev
+            result_args["optimality"] = float(result.optimality)
+            result_args["number_of_data_points"] = result.fun.size
+            result_args["number_of_parameters"] = result.x.size
+            result_args["degrees_of_freedom"] = (
+                result_args["number_of_data_points"] - result_args["number_of_parameters"]
+            )
+            result_args["chi_square"] = float(np.sum(result.fun**2))
+            result_args["reduced_chi_square"] = (
+                result_args["chi_square"] / result_args["degrees_of_freedom"]
+            )
+            result_args["root_mean_square_error"] = float(
+                np.sqrt(result_args["reduced_chi_square"])
+            )
+            result_args["jacobian"] = result.jac
+            result_args[
+                "covariance_matrix"
+            ] = self.calculate_covariance_matrix_and_standard_errors(
+                result_args["jacobian"], result_args["root_mean_square_error"]
+            )
+
+        return cls(**result_args)
+
+    def calculate_parameter_errors(self, parameters: Parameters):
+        standard_errors = self.root_mean_square_error * np.sqrt(np.diag(self.covariance_matrix))
+        for label, error in zip(self.free_parameter_labels, standard_errors):
+            self._parameters.get(label).standard_error = error
+
+
+def calculate_covariance_matrix_and_standard_errors(
+    jacobian: np.typing.ArrayLike, root_mean_square_error: float
+) -> np.typing.ArrayLike:
+    """Calculate the covariance matrix and standard errors of the optimization.
+
+    Parameters
+    ----------
+    jacobian : np.typing.ArrayLike
+        The jacobian matrix.
+    root_mean_square_error : float
+        The root mean square error.
+
+    Returns
+    -------
+    np.typing.ArrayLike
+        The covariance matrix.
+    """
+    # See PR #706: More robust covariance matrix calculation
+    _, jacobian_sv, jacobian_rsv = np.linalg.svd(jacobian, full_matrices=False)
+    jacobian_sv_square = jacobian_sv**2
+    mask = jacobian_sv_square > np.finfo(float).eps
+    covariance_matrix = (jacobian_rsv[mask].T / jacobian_sv_square[mask]) @ jacobian_rsv[mask]
+    return covariance_matrix
