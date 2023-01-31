b61b5ec5c2251d16be79e51da9102368ed550f38
Joern Weissenborn <joern.weissenborn@gmail.com>
2023-01-31 21:34:32 +0100

    Added test for multiple experiments.

--- glotaran/optimization/test/test_optimization_new.py
+++ glotaran/optimization/test/test_optimization_new.py
@@ -43,6 +43,44 @@ def test_single_data():
             assert np.allclose(param.value, parameters.get(param.label).value, rtol=1e-1)
 
 
+def test_multiple_experiments():
+    data_model = DataModel(megacomplex=["decay_independent"])
+    experiments = [
+        ExperimentModel(datasets={"decay_independent_1": data_model}),
+        ExperimentModel(datasets={"decay_independent_2": data_model}),
+    ]
+    parameters = Parameters.from_dict({"rates": {"decay": [0.8, 0.04]}})
+
+    global_axis = np.arange(10)
+    model_axis = np.arange(0, 150, 1)
+    clp = xr.DataArray(
+        [[1, 10]] * global_axis.size,
+        coords=(("global", global_axis), ("clp_label", ["c1", "c2"])),
+    )
+    data_model.data = simulate(
+        data_model, TestLibrary, parameters, {"global": global_axis, "model": model_axis}, clp
+    )
+
+    initial_parameters = Parameters.from_dict({"rates": {"decay": [0.9, 0.02]}})
+    print(initial_parameters)
+    optimization = Optimization(
+        experiments,
+        initial_parameters,
+        TestLibrary,
+        raise_exception=True,
+        maximum_number_function_evaluations=10,
+    )
+    optimized_parameters, optimized_data, result = optimization.run()
+    assert "decay_independent_1" in optimized_data
+    assert "decay_independent_2" in optimized_data
+    print(optimized_parameters)
+    assert result.success
+    assert initial_parameters != optimized_parameters
+    for param in optimized_parameters.all():
+        if param.vary:
+            assert np.allclose(param.value, parameters.get(param.label).value, rtol=1e-1)
+
+
 def test_global_data():
     data_model = DataModel(megacomplex=["decay_independent"], global_megacomplex=["gaussian"])
     experiment = ExperimentModel(datasets={"decay_independent": data_model})
