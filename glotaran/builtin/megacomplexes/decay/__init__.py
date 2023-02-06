a2359f390bee4157917fa2e2700dfc8d9ac3d135
Joern Weissenborn <joern.weissenborn@gmail.com>
2023-02-07 00:08:37 +0100

    Removed decay megacomplex

--- glotaran/builtin/megacomplexes/decay/__init__.py
+++
@@ -1,7 +0,0 @@
-from glotaran.builtin.megacomplexes.decay.decay_megacomplex import DecayMegacomplex
-from glotaran.builtin.megacomplexes.decay.decay_parallel_megacomplex import (
-    DecayParallelMegacomplex,
-)
-from glotaran.builtin.megacomplexes.decay.decay_sequential_megacomplex import (
-    DecaySequentialMegacomplex,
-)
