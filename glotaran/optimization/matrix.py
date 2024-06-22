🩹 Fix scaling for linked dataset case (#1461)
* 🩹Avoid scaling (raw) data with dataset scaling
* 🩹 Fix missing matrix scaling in OptimizationMatrix.from_linked_data
* 🧪🩹 Adapt unit tests to matrix being scaled instead of dataset

---------

Co-authored-by: s-weigand <s.weigand.phy@gmail.com>

glotaran/optimization/matrix.py
@@ -196,7 +196,7 @@ class OptimizationMatrix:
         return [
             cls.link(
                 [
-                    matrices[label].at_index(index)
+                    matrices[label].at_index(index).scale(linked_data.scales[label])
                     for label, index in zip(
                         linked_data.group_definitions[linked_data.group_labels[global_index]],
                         linked_data.data_indices[global_index],
@@ -268,12 +268,12 @@ class OptimizationMatrix:
             self.array = self.array[np.newaxis, :, :] * weight.T[:, :, np.newaxis]
         return self
 
-    def scale(self, scale: float) -> OptimizationMatrix:
+    def scale(self, scale: float | Parameter) -> OptimizationMatrix:
         """Create a matrix container with a scaled matrix.
 
         Parameters
         ----------
-        scale : float
+        scale : float | Parameter
             The scale.
 
         Returns

tests/optimization/test_matrix.py
@@ -115,7 +115,7 @@ def test_from_linked_data():
     assert np.array_equal(matrices[2].array, matrix_one.array)
 
     assert matrices[1].clp_axis == matrix_two.clp_axis
-    assert np.array_equal(matrices[1].array, matrix_two.at_index(1).array)
+    assert np.array_equal(matrices[1].array, matrix_two.at_index(1).array * 4)
 
     assert matrices[0].clp_axis == matrix_one.clp_axis + matrix_two.clp_axis
     print(matrices[0].array[0 : matrix_one.model_axis_size, 0], matrix_one.array[:, 0])
@@ -126,5 +126,5 @@ def test_from_linked_data():
         matrices[0].array[
             matrix_one.model_axis_size : matrix_one.model_axis_size + matrix_two.model_axis_size, 1
         ],
-        matrix_two.at_index(0).array[:, 0],
+        matrix_two.at_index(0).array[:, 0] * 4,
     )