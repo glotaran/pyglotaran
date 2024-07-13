🩹 Fix 1474 - add fields to result object - correct calculation of number of clps. (#1484)
* Rename nr_clp to number_of_clps
* Add additional penalty to result.
* Correct calculation of number of clps / degrees of freedom - this also has an affect on quantities that are partially dependent on it, such as RMSE.
---------

Co-authored-by: Joris Snellenburg <jsnel@users.noreply.github.com>

glotaran/optimization/objective.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -17,6 +18,14 @@ from glotaran.parameter.parameter import Parameter
 if TYPE_CHECKING:
     from glotaran.model.experiment_model import ExperimentModel
     from glotaran.typing.types import ArrayLike
+
+
+@dataclass
+class OptimizationObjectiveResult:
+    data: dict[str, xr.Dataset]
+    free_clp_size: int
+    additional_penalty: float
+    dataset_penalty: dict[str, float]
 
 
 class OptimizationObjective:
@@ -90,7 +99,7 @@ class OptimizationObjective:
             )
         return np.concatenate(penalties)
 
-    def get_result(self) -> dict[str, xr.Dataset]:
+    def get_result(self) -> OptimizationObjectiveResult:
         return (
             self.create_unlinked_result()
             if isinstance(self._data, OptimizationData)
@@ -268,7 +277,7 @@ class OptimizationObjective:
                 data.model, dataset, True
             )
 
-    def create_linked_result(self) -> dict[str, xr.Dataset]:
+    def create_linked_result(self) -> OptimizationObjectiveResult:
         assert isinstance(self._data, LinkedOptimizationData)
         matrices = {
             label: OptimizationMatrix.from_data(data) for label, data in self._data.data.items()
@@ -276,6 +285,7 @@ class OptimizationObjective:
         linked_matrices = OptimizationMatrix.from_linked_data(self._data, matrices)
         clp_axes = [matrix.clp_axis for matrix in linked_matrices]
         reduced_matrices = self.calculate_reduced_matrices(linked_matrices)
+        free_clp_size = sum(len(matrix.clp_axis) for matrix in reduced_matrices)
         estimations = self.resolve_estimations(
             linked_matrices,
             reduced_matrices,
@@ -291,9 +301,20 @@ class OptimizationObjective:
                 results[label], label, clp_axes, estimations
             )
             self.finalize_result_dataset(results[label], data)
-        return results
+        additional_penalty = sum(
+            calculate_clp_penalties(
+                linked_matrices,
+                estimations,
+                self._data.global_axis,
+                self._model.clp_penalties,
+            )
+        )
+        dataset_penalties = {label: d.residual.sum().data for label, d in results.items()}
+        return OptimizationObjectiveResult(
+            results, free_clp_size, additional_penalty, dataset_penalties
+        )
 
-    def create_unlinked_result(self) -> dict[str, xr.Dataset]:
+    def create_unlinked_result(self) -> OptimizationObjectiveResult:
         assert isinstance(self._data, OptimizationData)
 
         label = next(iter(self._model.datasets.keys()))
@@ -301,18 +322,33 @@ class OptimizationObjective:
 
         matrix = OptimizationMatrix.from_data(self._data)
         self.add_matrix_to_dataset(result, matrix)
+        additional_penalty = 0
         if self._data.is_global:
             self.add_global_clp_and_residual_to_dataset(result, self._data, matrix)
+            free_clp_size = len(matrix.clp_axis)
+
         else:
             reduced_matrices = self.calculate_reduced_matrices(
                 matrix.as_global_list(self._data.global_axis)
             )
+            free_clp_size = sum(len(matrix.clp_axis) for matrix in reduced_matrices)
             estimations = self.resolve_estimations(
                 matrix.as_global_list(self._data.global_axis),
                 reduced_matrices,
                 self.calculate_estimations(reduced_matrices),
             )
             self.add_unlinked_clp_and_residual_to_dataset(result, estimations)
+            additional_penalty = sum(
+                calculate_clp_penalties(
+                    [matrix],
+                    estimations,
+                    self._data.global_axis,
+                    self._model.clp_penalties,
+                )
+            )
 
         self.finalize_result_dataset(result, self._data)
-        return {label: result}
+        dataset_penalties = {label: result.residual.sum().data}
+        return OptimizationObjectiveResult(
+            {label: result}, free_clp_size, additional_penalty, dataset_penalties
+        )

glotaran/optimization/optimization.py
@@ -135,16 +135,19 @@ class Optimization:
                 termination_reason = str(e)
 
         penalty = np.concatenate([o.calculate() for o in self._objectives])
-        data = dict(ChainMap(*[o.get_result() for o in self._objectives]))
-        nr_clp = len({str(c.data) for d in data.values() for c in d.clp_label})
+        results = [o.get_result() for o in self._objectives]
+        data = dict(ChainMap(*[r.data for r in results]))
+        number_of_clps = sum(r.free_clp_size for r in results)
+        additional_penalty = sum(r.additional_penalty for r in results)
         result = OptimizationResult.from_least_squares_result(
             ls_result,
             self._parameter_history,
             OptimizationHistory.from_stdout_str(self._tee.read()),
             penalty,
+            additional_penalty,
             self._free_parameter_labels,
             termination_reason,
-            nr_clp,
+            number_of_clps,
         )
         return self._parameters, data, result
 
@@ -152,16 +155,19 @@ class Optimization:
         termination_reason = "Dry run."
 
         penalty = np.concatenate([o.calculate() for o in self._objectives])
-        data = dict(ChainMap(*[o.get_result() for o in self._objectives]))
-        nr_clp = len({str(c.data) for d in data.values() for c in d.clp_label})
+        results = [o.get_result() for o in self._objectives]
+        data = dict(ChainMap(*[r.data for r in results]))
+        number_of_clps = sum(r.free_clp_size for r in results)
+        additional_penalty = sum(r.additional_penalty for r in results)
         result = OptimizationResult.from_least_squares_result(
             None,
             self._parameter_history,
             OptimizationHistory.from_stdout_str(self._tee.read()),
             penalty,
+            additional_penalty,
             self._free_parameter_labels,
             termination_reason,
-            nr_clp,
+            number_of_clps,
         )
         return self._parameters, data, result
 

glotaran/optimization/result.py
@@ -61,7 +61,7 @@ class OptimizationResult(BaseModel):
 
     The rows and columns are corresponding to :attr:`free_parameter_labels`."""
 
-    number_clp: int | None = None
+    number_of_clps: int | None = None
     degrees_of_freedom: int | None = None
     """Degrees of freedom in optimization :math:`N - N_{vars}`."""
 
@@ -88,6 +88,7 @@ class OptimizationResult(BaseModel):
 
     :math:`rms = \sqrt{\chi^2_{red}}`
     """
+    additional_penalty: float | None = None
 
     @classmethod
     def from_least_squares_result(
@@ -96,9 +97,10 @@ class OptimizationResult(BaseModel):
         parameter_history: ParameterHistory,
         optimization_history: OptimizationHistory,
         penalty: ArrayLike,
+        additional_penalty: float,
         free_parameter_labels: list[str],
         termination_reason: str,
-        number_clp: int,
+        number_of_clps: int,
     ):
         success = result is not None
 
@@ -115,7 +117,8 @@ class OptimizationResult(BaseModel):
         }
 
         if success:
-            result_args["number_clp"] = number_clp
+            result_args["number_of_clps"] = number_of_clps
+            result_args["additional_penalty"] = additional_penalty
             result_args["number_of_jacobian_evaluations"] = result.njev  # type:ignore[union-attr]
             result_args["optimality"] = float(result.optimality)  # type:ignore[union-attr]
             result_args["number_of_data_points"] = result.fun.size  # type:ignore[union-attr]
@@ -123,7 +126,7 @@ class OptimizationResult(BaseModel):
             result_args["degrees_of_freedom"] = (
                 result_args["number_of_data_points"]
                 - result_args["number_of_parameters"]
-                - result_args["number_clp"]
+                - result_args["number_of_clps"]
             )
             result_args["chi_square"] = float(np.sum(result.fun**2))  # type:ignore[union-attr]
             result_args["reduced_chi_square"] = (