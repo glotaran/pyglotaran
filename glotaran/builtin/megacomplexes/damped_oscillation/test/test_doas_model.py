a12f5c929e83383adc4910c6c84af9e1ff4876ed
Joris Snellenburg <jsnel@users.noreply.github.com>
2024-08-23 19:23:53 +0200

    👌 Fixed some typos

--- glotaran/builtin/megacomplexes/damped_oscillation/test/test_doas_model.py
+++ glotaran/builtin/megacomplexes/damped_oscillation/test/test_doas_model.py
@@ -389,9 +389,9 @@ def test_doas_model(suite):
 
 
 def test_doas_model_validate():
-    """A ``OscillationParameterIssue`` should be raise if there is list length mismatch.
+    """An ``OscillationParameterIssue`` should be raised if there is a list length mismatch.
 
-    List values are: ``labels``, ``frequencies``, ``rates``
+    List values are: ``labels``, ``frequencies``, ``rates``.
     """
     model_data = OneOscillation.sim_model.as_dict()
     model_data["megacomplex"]["m1"]["labels"].append("extra-label")
@@ -399,6 +399,6 @@ def test_doas_model_validate():
     validation_msg = model.validate()
     assert (
         validation_msg == "Your model has 1 problem:\n\n"
-        " * Size of labels (2), frequencies (1) and rates (1) does not match for damped "
+        " * The size of labels (2), frequencies (1), and rates (1) does not match for damped "
         "oscillation megacomplex 'm1'."
     )
