dcd45edd0356bbf90b4ee1f2b586e2c860378bd2
Joris Snellenburg <jsnel@users.noreply.github.com>
2024-08-25 16:26:44 +0200

    🩹Fix prepare_dataset's add_svd_to_dataset function
    Use raw numpy array instead of xarray dataarray by accessing the dataarray's .data attribute. This resolved an issues with numpy>2.

--- glotaran/io/prepare_dataset.py
+++ glotaran/io/prepare_dataset.py
@@ -85,7 +85,7 @@ def add_svd_to_dataset(
     if data_array is None:
         data_array = dataset[name] if name != "data" else dataset.data
     if f"{name}_singular_values" not in dataset:
-        lsv, sv, rsv = np.linalg.svd(data_array, full_matrices=False)
+        lsv, sv, rsv = np.linalg.svd(data_array.data, full_matrices=False)
         dataset[f"{name}_left_singular_vectors"] = ((lsv_dim, "left_singular_value_index"), lsv)
         dataset[f"{name}_singular_values"] = (("singular_value_index"), sv)
         dataset[f"{name}_right_singular_vectors"] = (
