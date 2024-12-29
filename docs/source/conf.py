31b2151140a5eb630f3cd05db67ce706e05f3b84
Sebastian Weigand <s.weigand.phy@gmail.com>
2024-12-29 22:20:36 +0100

    🧹 Fix docs conf deprecation warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,7 +78,7 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = [".rst", ".md"]
+source_suffix = {".rst": "restructuredtext", ".md": "restructuredtext"}
 # source_suffix = ".rst"
 
 linkcheck_ignore = [
@@ -95,7 +95,7 @@ imgmath_image_format = "svg"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
